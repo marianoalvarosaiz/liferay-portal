011f43686f9329ab6f36962c68b75161f5468879
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2017-07-03 09:31:43 +0200

    Compare number of objects.

--- portal-test/src/com/liferay/portal/kernel/test/rule/callback/CheckIndexerTestCallback.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/callback/CheckIndexerTestCallback.java
@@ -31,18 +31,24 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.runner.Description;
 
 /**
  * @author Mariano Alvaro Saiz
  */
-public class CheckIndexerTestCallback extends BaseTestCallback<Object, Object> {
+public class CheckIndexerTestCallback
+	extends BaseTestCallback<List<Integer>, Object> {
 
 	public static final CheckIndexerTestCallback INSTANCE =
 		new CheckIndexerTestCallback();
 
-	@Override
-	public void afterClass(Description description, Object c) throws Throwable {
+	public void afterCla(Description description, Object c) throws Throwable {
 		Registry registry = RegistryUtil.getRegistry();
 
 		IndexerRegistry indexerRegistry = registry.getService(
@@ -75,6 +81,31 @@ public class CheckIndexerTestCallback extends BaseTestCallback<Object, Object> {
 		}
 	}
 
+	@Override
+	public void afterClass(Description description, List<Integer> beforeLengths)
+		throws Throwable {
+
+		List<Integer> afterLengths = _getIndexesLengths();
+
+		StringBundler sb = new StringBundler();
+
+		for (int i = 0; i < beforeLengths.size(); i++) {
+			if (beforeLengths.get(i) < afterLengths.get(i)) {
+				sb.append(" Not deleted: ");
+				sb.append(i);
+			}
+		}
+
+		if (sb.length() > 0) {
+			throw new IllegalStateException(sb.toString());
+		}
+	}
+
+	@Override
+	public List<Integer> beforeClass(Description description) throws Throwable {
+		return _getIndexesLengths();
+	}
+
 	protected SearchContext getSearchContext() throws Exception {
 		SearchContext searchContext = new SearchContext();
 
@@ -92,7 +123,7 @@ public class CheckIndexerTestCallback extends BaseTestCallback<Object, Object> {
 		return userLocalService.getUser(userId);
 	}
 
-	protected Hits search(Indexer<User> indexer, SearchContext searchContext)
+	protected Hits search(Indexer<?> indexer, SearchContext searchContext)
 		throws Exception {
 
 		return indexer.search(searchContext);
@@ -111,7 +142,33 @@ public class CheckIndexerTestCallback extends BaseTestCallback<Object, Object> {
 	private CheckIndexerTestCallback() {
 	}
 
+	private List<Integer> _getIndexesLengths() throws Exception {
+		Registry registry = RegistryUtil.getRegistry();
+
+		IndexerRegistry indexerRegistry = registry.getService(
+			IndexerRegistry.class);
+
+		List<Integer> lengths = new ArrayList<>();
+
+		for (Class<?> clazz : _orderedClasses) {
+			Indexer<?> indexer = indexerRegistry.getIndexer(clazz);
+
+			SearchContext searchContext = getSearchContext();
+
+			searchContext.setKeywords(StringPool.BLANK);
+
+			Hits hits = search(indexer, searchContext);
+
+			lengths.add(hits.getLength());
+		}
+
+		return lengths;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		CheckIndexerTestCallback.class);
+
+	private static final Set<Class<?>> _orderedClasses = new LinkedHashSet<>(
+		Arrays.<Class<?>>asList(User.class));
 
 }
