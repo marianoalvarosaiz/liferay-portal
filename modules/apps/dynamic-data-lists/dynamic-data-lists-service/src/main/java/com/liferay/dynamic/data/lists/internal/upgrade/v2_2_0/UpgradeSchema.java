eee9122a9651e65b83da0797a3f002a1964884f7
Alberto Chaparro <alberto.chaparro@liferay.com>
2020-04-09 18:21:02 -0700

    LPS-78233 Missing case

--- modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/v2_2_0/UpgradeSchema.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-service/src/main/java/com/liferay/dynamic/data/lists/internal/upgrade/v2_2_0/UpgradeSchema.java
@@ -24,17 +24,11 @@ public class UpgradeSchema extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		if (!hasColumn("DDLRecord", "className")) {
-			alter(
-				DDLRecordTable.class,
-				new AlterTableAddColumn("className", "VARCHAR(300) null"));
-		}
+		alter(
+			DDLRecordTable.class,
+			new AlterTableAddColumn("className", "VARCHAR(300) null"));
 
-		if (!hasColumn("DDLRecord", "classPK")) {
-			alter(
-				DDLRecordTable.class,
-				new AlterTableAddColumn("classPK", "LONG"));
-		}
+		alter(DDLRecordTable.class, new AlterTableAddColumn("classPK", "LONG"));
 	}
 
 }
