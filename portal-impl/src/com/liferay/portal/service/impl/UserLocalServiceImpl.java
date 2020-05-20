d8168bf1295edd50a03d4a6a2a695f9467ee110f
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2020-05-20 18:21:39 +0200

    Revert "LPS-102980 Do not use control panel plid, if not it will redirect to login page. Use same logic as in sendEmailAddressVerification."
    This reverts commit 4261f8e9ad5e148765fb8e5daa13536cacf10963.

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -6009,26 +6009,10 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 				user.getCompanyId(), User.class.getName(), user.getUserId(),
 				TicketConstants.TYPE_PASSWORD, null, null, serviceContext);
 
-			String updatePasswordURL = "/portal/update_password?";
-
-			long plid = serviceContext.getPlid();
-
-			if (plid > 0) {
-				Layout layout = layoutLocalService.fetchLayout(plid);
-
-				if (layout != null) {
-					Group group = layout.getGroup();
-
-					if (!layout.isPrivateLayout() && !group.isUser()) {
-						updatePasswordURL +=
-							"p_l_id=" + serviceContext.getPlid() + "&";
-					}
-				}
-			}
-
 			passwordResetURL = StringBundler.concat(
 				serviceContext.getPortalURL(), serviceContext.getPathMain(),
-				updatePasswordURL, "ticketKey=", ticket.getKey());
+				"/portal/update_password?p_l_id=", serviceContext.getPlid(),
+				"&ticketKey=", ticket.getKey());
 
 			localizedBodyMap = LocalizationUtil.getLocalizationMap(
 				companyPortletPreferences,
