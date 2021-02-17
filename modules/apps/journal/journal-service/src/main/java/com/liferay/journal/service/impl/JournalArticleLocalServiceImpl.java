6b9fd1437cffdb420db67faa541aedd7f26c5595
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-02-17 08:51:21 +0100

    LPS-125162 Rename inner class.

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7650,10 +7650,7 @@ public class JournalArticleLocalServiceImpl
 		displayDateLT = CalendarUtil.getTimestamp(displayDateLT);
 		reviewDate = CalendarUtil.getTimestamp(reviewDate);
 
-		DSLQueryByC_G_F_C_A_V_T_D_C_S_T_D_S_R_L dslQueryGenerator =
-			new DSLQueryByC_G_F_C_A_V_T_D_C_S_T_D_S_R_L();
-
-		DSLQuery dslQuery = dslQueryGenerator.searchCountQuery(
+		DSLQuery dslQuery = _getSearchCountDSLQuery(
 			companyId, groupId, folderIds, classNameId, articleIds, version,
 			titles, descriptions, contents, ddmStructureKeys, ddmTemplateKeys,
 			displayDateGT, displayDateLT, reviewDate, locale, andOperator,
@@ -7683,10 +7680,7 @@ public class JournalArticleLocalServiceImpl
 		displayDateLT = CalendarUtil.getTimestamp(displayDateLT);
 		reviewDate = CalendarUtil.getTimestamp(reviewDate);
 
-		DSLQueryByC_G_F_C_A_V_T_D_C_S_T_D_S_R_L dslQueryGenerator =
-			new DSLQueryByC_G_F_C_A_V_T_D_C_S_T_D_S_R_L();
-
-		DSLQuery dslQuery = dslQueryGenerator.searchQuery(
+		DSLQuery dslQuery = _getSearchDSLQuery(
 			companyId, groupId, folderIds, classNameId, articleIds, version,
 			titles, descriptions, contents, ddmStructureKeys, ddmTemplateKeys,
 			displayDateGT, displayDateLT, reviewDate, locale, andOperator,
@@ -9352,6 +9346,42 @@ public class JournalArticleLocalServiceImpl
 		return (JournalArticleModelValidator)modelValidator;
 	}
 
+	private DSLQuery _getSearchCountDSLQuery(
+		long companyId, long groupId, List<Long> folderIds, long classNameId,
+		String[] articleIds, Double version, String[] titles,
+		String[] descriptions, String[] contents, String[] ddmStructureKeys,
+		String[] ddmTemplateKeys, Date displayDateGT, Date displayDateLT,
+		Date reviewDate, Locale locale, boolean andOperator,
+		QueryDefinition<JournalArticle> queryDefinition,
+		boolean inlineSQLHelper) {
+
+		SearchDSLQuery searchDSLQuery = new SearchDSLQuery();
+
+		return searchDSLQuery.getSearchCountQuery(
+			companyId, groupId, folderIds, classNameId, articleIds, version,
+			titles, descriptions, contents, ddmStructureKeys, ddmTemplateKeys,
+			displayDateGT, displayDateLT, reviewDate, locale, andOperator,
+			queryDefinition, inlineSQLHelper);
+	}
+
+	private DSLQuery _getSearchDSLQuery(
+		long companyId, long groupId, List<Long> folderIds, long classNameId,
+		String[] articleIds, Double version, String[] titles,
+		String[] descriptions, String[] contents, String[] ddmStructureKeys,
+		String[] ddmTemplateKeys, Date displayDateGT, Date displayDateLT,
+		Date reviewDate, Locale locale, boolean andOperator,
+		QueryDefinition<JournalArticle> queryDefinition,
+		boolean inlineSQLHelper) {
+
+		SearchDSLQuery searchDSLQuery = new SearchDSLQuery();
+
+		return searchDSLQuery.getSearchQuery(
+			companyId, groupId, folderIds, classNameId, articleIds, version,
+			titles, descriptions, contents, ddmStructureKeys, ddmTemplateKeys,
+			displayDateGT, displayDateLT, reviewDate, locale, andOperator,
+			queryDefinition, inlineSQLHelper);
+	}
+
 	private int _getUniqueUrlTitleCount(
 		long groupId, String articleId, String urlTitle) {
 
@@ -9553,9 +9583,9 @@ public class JournalArticleLocalServiceImpl
 	@Reference
 	private TrashVersionLocalService _trashVersionLocalService;
 
-	private class DSLQueryByC_G_F_C_A_V_T_D_C_S_T_D_S_R_L {
+	private class SearchDSLQuery {
 
-		public DSLQuery searchCountQuery(
+		public DSLQuery getSearchCountQuery(
 			long companyId, long groupId, List<Long> folderIds,
 			long classNameId, String[] articleIds, Double version,
 			String[] titles, String[] descriptions, String[] contents,
@@ -9584,7 +9614,7 @@ public class JournalArticleLocalServiceImpl
 			);
 		}
 
-		public DSLQuery searchQuery(
+		public DSLQuery getSearchQuery(
 			long companyId, long groupId, List<Long> folderIds,
 			long classNameId, String[] articleIds, Double version,
 			String[] titles, String[] descriptions, String[] contents,
