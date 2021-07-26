ddeff005816980e0c12c4a2dbc658f76b6fb72e6
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-07-26 14:29:23 +0200

    LPS-133366 Honor database.in.max.parameters if CTPersistenceHelperUtil.isProductionMode is false.

--- modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/service/persistence/impl/test/CacheMissPersistenceImplTest.java
+++ modules/util/portal-tools-service-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/service/builder/test/service/persistence/impl/test/CacheMissPersistenceImplTest.java
@@ -15,6 +15,8 @@
 package com.liferay.portal.tools.service.builder.test.service.persistence.impl.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.service.persistence.change.tracking.helper.CTPersistenceHelper;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -22,19 +24,30 @@ import com.liferay.portal.test.rule.TransactionalTestRule;
 import com.liferay.portal.tools.service.builder.test.model.CacheMissEntry;
 import com.liferay.portal.tools.service.builder.test.service.persistence.CacheMissEntryPersistence;
 import com.liferay.portal.tools.service.builder.test.service.persistence.CacheMissEntryUtil;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
 
 import java.io.Serializable;
 
+import java.util.Dictionary;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.Set;
 
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
 
 /**
  * @author Minhchau Dang
@@ -51,13 +64,25 @@ public class CacheMissPersistenceImplTest {
 				Propagation.REQUIRED,
 				"com.liferay.portal.tools.service.builder.test.service"));
 
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_serviceRegistration = _registerCTPersistenceHelper();
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		_serviceRegistration.unregister();
+	}
+
 	@Before
 	public void setUp() {
 		_persistence = CacheMissEntryUtil.getPersistence();
 	}
 
 	@Test
-	public void testCacheMiss() throws Throwable {
+	public void testCacheMissIfCTProductionModeDisabled() throws Throwable {
+		_setProductionMode(false);
+
 		Set<Serializable> primaryKeys = new HashSet<>();
 
 		for (long pk = -1; pk > -2000; pk--) {
@@ -70,6 +95,83 @@ public class CacheMissPersistenceImplTest {
 		Assert.assertTrue(cacheMissEntries.isEmpty());
 	}
 
+	@Test
+	public void testCacheMissIfCTProductionModeEnabled() throws Throwable {
+		_setProductionMode(true);
+
+		Set<Serializable> primaryKeys = new HashSet<>();
+
+		for (long pk = -1; pk > -2000; pk--) {
+			primaryKeys.add(pk);
+		}
+
+		Map<Serializable, CacheMissEntry> cacheMissEntries =
+			_persistence.fetchByPrimaryKeys(primaryKeys);
+
+		Assert.assertTrue(cacheMissEntries.isEmpty());
+	}
+
+	private static ServiceRegistration<CTPersistenceHelper>
+			_registerCTPersistenceHelper()
+		throws Exception {
+
+		Registry registry = RegistryUtil.getRegistry();
+
+		CTPersistenceHelper ctPersistenceHelper = registry.getService(
+			registry.getServiceReference(CTPersistenceHelper.class));
+
+		Bundle bundle = FrameworkUtil.getBundle(ctPersistenceHelper.getClass());
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		Dictionary<String, Object> properties = new Hashtable<>();
+
+		properties.put("service.ranking", 100);
+
+		return bundleContext.registerService(
+			CTPersistenceHelper.class,
+			new CTPersistenceHelperProductionModeAware(ctPersistenceHelper),
+			properties);
+	}
+
+	private void _setProductionMode(boolean productionMode) {
+		_productionMode = productionMode;
+	}
+
+	private static boolean _productionMode;
+	private static ServiceRegistration<CTPersistenceHelper>
+		_serviceRegistration;
+
 	private CacheMissEntryPersistence _persistence;
+
+	private static class CTPersistenceHelperProductionModeAware
+		implements CTPersistenceHelper {
+
+		public CTPersistenceHelperProductionModeAware(
+			CTPersistenceHelper ctPersistenceHelper) {
+
+			_ctPersistenceHelper = ctPersistenceHelper;
+		}
+
+		@Override
+		public <T extends CTModel<T>> boolean isInsert(T ctModel) {
+			return _ctPersistenceHelper.isInsert(ctModel);
+		}
+
+		@Override
+		public <T extends CTModel<T>> boolean isProductionMode(
+			Class<T> ctModelClass) {
+
+			return _productionMode;
+		}
+
+		@Override
+		public <T extends CTModel<T>> boolean isRemove(T ctModel) {
+			return _ctPersistenceHelper.isRemove(ctModel);
+		}
+
+		private final CTPersistenceHelper _ctPersistenceHelper;
+
+	}
 
 }
