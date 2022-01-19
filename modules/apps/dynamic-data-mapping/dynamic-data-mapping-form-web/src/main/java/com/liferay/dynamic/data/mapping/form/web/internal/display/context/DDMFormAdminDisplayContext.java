df3442e46d9f805255083b798e71410c385c0db9
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-01-19 19:11:10 +0100

    LPS-138464 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/display/context/DDMFormAdminDisplayContext.java
@@ -143,7 +143,6 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.PageContext;
 
 /**
@@ -650,11 +649,9 @@ public class DDMFormAdminDisplayContext {
 	public String getEmptyResultsMessage() {
 		SearchContainer<?> search = getSearch();
 
-		HttpServletRequest httpServletRequest =
-			ddmFormAdminRequestHelper.getRequest();
-
 		return LanguageUtil.get(
-			httpServletRequest, search.getEmptyResultsMessage());
+			ddmFormAdminRequestHelper.getRequest(),
+			search.getEmptyResultsMessage());
 	}
 
 	public String getFieldSetDefinitionURL() throws PortalException {
@@ -1102,9 +1099,6 @@ public class DDMFormAdminDisplayContext {
 			_getDDMFormInstanceOrderByComparator(
 				getOrderByCol(), getOrderByType()));
 		ddmFormInstanceSearch.setOrderByType(getOrderByType());
-		ddmFormInstanceSearch.setRowChecker(
-			new DDMFormInstanceRowChecker(renderResponse));
-
 		ddmFormInstanceSearch.setResultsAndTotal(
 			() -> _ddmFormInstanceService.search(
 				ddmFormAdminRequestHelper.getCompanyId(),
@@ -1115,6 +1109,8 @@ public class DDMFormAdminDisplayContext {
 			_ddmFormInstanceService.searchCount(
 				ddmFormAdminRequestHelper.getCompanyId(),
 				ddmFormAdminRequestHelper.getScopeGroupId(), getKeywords()));
+		ddmFormInstanceSearch.setRowChecker(
+			new DDMFormInstanceRowChecker(renderResponse));
 
 		return ddmFormInstanceSearch;
 	}
@@ -1578,17 +1574,12 @@ public class DDMFormAdminDisplayContext {
 
 		ThemeDisplay themeDisplay = ddmFormAdminRequestHelper.getThemeDisplay();
 
-		long fieldSetClassNameId = PortalUtil.getClassNameId(
-			DDMFormInstance.class);
-
-		DDMFormBuilderSettingsRequest ddmFormBuilderSettingsRequest =
-			DDMFormBuilderSettingsRequest.with(
-				themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId(),
-				fieldSetClassNameId, getDDMForm(), themeDisplay.getLocale());
-
 		_ddmFormBuilderSettingsResponse =
 			_ddmFormBuilderSettingsRetriever.getSettings(
-				ddmFormBuilderSettingsRequest);
+				DDMFormBuilderSettingsRequest.with(
+					themeDisplay.getCompanyId(), themeDisplay.getScopeGroupId(),
+					PortalUtil.getClassNameId(DDMFormInstance.class),
+					getDDMForm(), themeDisplay.getLocale()));
 
 		return _ddmFormBuilderSettingsResponse;
 	}
