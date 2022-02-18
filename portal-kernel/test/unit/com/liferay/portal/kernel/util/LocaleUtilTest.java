9e1f9c4a4ece90bb6bdc26031b1b7befa439fc67
Tina Tian <tina.tian@liferay.com>
2022-02-18 15:18:34 -0800

    LPS-145927 Fix unit test, add null checking to make sure that locale is created

--- portal-kernel/test/unit/com/liferay/portal/kernel/util/LocaleUtilTest.java
+++ portal-kernel/test/unit/com/liferay/portal/kernel/util/LocaleUtilTest.java
@@ -146,6 +146,8 @@ public class LocaleUtilTest extends PowerMockito {
 			false
 		);
 
+		Assert.assertNotNull(LocaleUtil.fromLanguageId("it_IT", false, false));
+
 		Assert.assertSame(
 			LocaleUtil.fromLanguageId("it_IT", false, false),
 			LocaleUtil.fromLanguageId("it_IT", false, false));
