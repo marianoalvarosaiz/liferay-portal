813d2b69f1875f111af761b5188c2e9b90bd30ae
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-03-14 13:46:52 +0100

    LPS-138464 Apply newly created method.

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
@@ -441,10 +441,7 @@ public class AssetPublisherDisplayContext {
 
 		SearchContainer<AssetEntry> searchContainer = getSearchContainer();
 
-		searchContainer.setResultsAndTotal(
-			() -> assetEntries.subList(
-				searchContainer.getStart(), searchContainer.getResultEnd()),
-			assetEntries.size());
+		searchContainer.setResultsAndTotal(assetEntries);
 
 		List<AssetEntryResult> assetEntryResults = new ArrayList<>();
 
