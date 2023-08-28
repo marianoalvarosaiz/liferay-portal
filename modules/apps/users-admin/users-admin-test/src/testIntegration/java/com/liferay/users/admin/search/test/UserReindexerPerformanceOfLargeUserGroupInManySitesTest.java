76eff0926536ce577ef163a105dc45d48de6d248
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2023-08-28 12:08:46 +0200

    LPS-189621 Create test to expose parameter limit failure in SQL Server.

--- modules/apps/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/search/test/UserReindexerPerformanceOfLargeUserGroupInManySitesTest.java
+++ modules/apps/users-admin/users-admin-test/src/testIntegration/java/com/liferay/users/admin/search/test/UserReindexerPerformanceOfLargeUserGroupInManySitesTest.java
@@ -145,6 +145,13 @@ public class UserReindexerPerformanceOfLargeUserGroupInManySitesTest {
 			).build());
 	}
 
+	@Test
+	public void testReindexUserNumberAboveParameterLimitation()
+		throws Exception {
+
+		reindex(TestPropsValues.getCompanyId(), new long[3000]);
+	}
+
 	protected Group addGroup() {
 		return groupSearchFixture.addGroup(new GroupBlueprint());
 	}
@@ -282,9 +289,13 @@ public class UserReindexerPerformanceOfLargeUserGroupInManySitesTest {
 	protected void reindex(List<User> users) {
 		User user = users.get(0);
 
-		_reindexer.reindex(
-			user.getCompanyId(), _CLASS_NAME,
+		reindex(
+			user.getCompanyId(),
 			TransformUtil.transformToLongArray(users, User::getUserId));
+	}
+
+	protected void reindex(long companyId, long... classPKs) {
+		_reindexer.reindex(companyId, _CLASS_NAME, classPKs);
 	}
 
 	protected SearchResponse searchUsersInAllGroups(
