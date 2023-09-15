01f13458daa7ffce80d261b1c8280ba17f582678
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2023-09-15 09:28:20 +0200

    LPS-189621 For SQL Server parameter limitation handling can not be done at restriction level since the restriction applies to the parameter total, not to each IN clause subtotal.
    So, for SQL Server, the reindexation will be split in subqueries. For the other databases the DATABASE_IN_MAX_PARAMETERS restriction can be applied at RestrictionsFactoryUtil level so there is no need to split in different IndexableActionableDynamicQuery.

--- modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/UserBulkReindexer.java
+++ modules/apps/users-admin/users-admin-impl/src/main/java/com/liferay/users/admin/internal/search/UserBulkReindexer.java
@@ -5,6 +5,10 @@
 
 package com.liferay.users.admin.internal.search;
 
+import com.liferay.portal.kernel.configuration.Filter;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -13,10 +17,16 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.search.spi.reindexer.BulkReindexer;
 
 import java.util.Collection;
+import java.util.List;
 
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -31,6 +41,49 @@ public class UserBulkReindexer implements BulkReindexer {
 
 	@Override
 	public void reindex(long companyId, Collection<Long> classPKs) {
+		int size = classPKs.size();
+
+		if (size <= _databaseMaxParameters) {
+			_reindex(companyId, classPKs);
+
+			return;
+		}
+
+		List<Long> classPKsList = ListUtil.fromCollection(classPKs);
+
+		int start = 0;
+		int end = _databaseMaxParameters;
+
+		while (start < size) {
+			_reindex(companyId, ListUtil.subList(classPKsList, start, end));
+
+			end += _databaseMaxParameters;
+			start += _databaseMaxParameters;
+		}
+	}
+
+	@Activate
+	protected void activate() {
+		DB db = DBManagerUtil.getDB();
+
+		DBType dbType = db.getDBType();
+
+		_databaseMaxParameters = GetterUtil.getInteger(
+			PropsUtil.get(
+				PropsKeys.DATABASE_MAX_PARAMETERS,
+				new Filter(dbType.getName())),
+			Integer.MAX_VALUE);
+	}
+
+	@Reference(
+		target = "(indexer.class.name=com.liferay.portal.kernel.model.User)"
+	)
+	protected Indexer<User> indexer;
+
+	@Reference
+	protected UserLocalService userLocalService;
+
+	private void _reindex(long companyId, Collection<Long> classPKs) {
 		IndexableActionableDynamicQuery indexableActionableDynamicQuery =
 			userLocalService.getIndexableActionableDynamicQuery();
 
@@ -63,15 +116,9 @@ public class UserBulkReindexer implements BulkReindexer {
 		}
 	}
 
-	@Reference(
-		target = "(indexer.class.name=com.liferay.portal.kernel.model.User)"
-	)
-	protected Indexer<User> indexer;
-
-	@Reference
-	protected UserLocalService userLocalService;
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		UserBulkReindexer.class);
+
+	private int _databaseMaxParameters;
 
 }
