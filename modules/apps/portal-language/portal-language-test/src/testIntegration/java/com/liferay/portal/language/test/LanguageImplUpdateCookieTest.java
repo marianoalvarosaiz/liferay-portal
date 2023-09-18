a977f546cfa52548d5cd6c376972c0b496bb2419
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2023-09-18 09:45:07 +0200

    LPS-196208 Create test to ensure that only one NAME_GUEST_LANGUAGE_ID cookie is added to the response when updating the cookie with a new locale.

---
+++ modules/apps/portal-language/portal-language-test/src/testIntegration/java/com/liferay/portal/language/test/LanguageImplUpdateCookieTest.java
@@ -0,0 +1,74 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.language.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.portal.kernel.cookies.constants.CookiesConstants;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import javax.servlet.http.Cookie;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+@RunWith(Arquillian.class)
+public class LanguageImplUpdateCookieTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testUpdateCookieGeneratesOnlyOneActiveCookie() {
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+		MockHttpServletResponse mockHttpServletResponse =
+			new MockHttpServletResponse();
+
+		_language.updateCookie(
+			mockHttpServletRequest, mockHttpServletResponse, LocaleUtil.US);
+		_language.updateCookie(
+			mockHttpServletRequest, mockHttpServletResponse, LocaleUtil.SPAIN);
+
+		Cookie validCookie = null;
+		int total = 0;
+
+		for (Cookie cookie : mockHttpServletResponse.getCookies()) {
+			if (StringUtil.equals(
+					cookie.getName(),
+					CookiesConstants.NAME_GUEST_LANGUAGE_ID)) {
+
+				validCookie = cookie;
+				total++;
+			}
+		}
+
+		Assert.assertEquals(1, total);
+
+		Assert.assertNotEquals(0, validCookie.getMaxAge());
+		Assert.assertEquals(
+			LocaleUtil.toLanguageId(LocaleUtil.SPAIN), validCookie.getValue());
+	}
+
+	@Inject
+	private static Language _language;
+
+}
