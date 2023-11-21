c007bab48af968bd82a477472b3408d694703dec
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2023-11-21 08:16:53 +0100

    LPS-201076 Move BundleManagerUtil to marketplace-api and apply.

--- modules/apps/marketplace/marketplace-api/src/main/java/com/liferay/marketplace/bundle/util/BundleManagerUtil.java
+++ modules/apps/marketplace/marketplace-api/src/main/java/com/liferay/marketplace/bundle/util/BundleManagerUtil.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.marketplace.internal.bundle;
+package com.liferay.marketplace.bundle.util;
 
 import com.liferay.marketplace.bundle.BundleManager;
 import com.liferay.portal.kernel.module.service.Snapshot;
