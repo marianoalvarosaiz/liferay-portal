fc334189b1926ffeb48ff78d22f464934535bcfa
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2023-11-24 15:07:55 +0100

    LPS-201076 Move BundleManagerUtil from service to api module and replace each use of BundleManager or different BundleManagerUtil.

--- modules/apps/marketplace/marketplace-api/src/main/java/com/liferay/marketplace/util/BundleManagerUtil.java
+++ modules/apps/marketplace/marketplace-api/src/main/java/com/liferay/marketplace/util/BundleManagerUtil.java
@@ -1,9 +1,9 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.marketplace.internal.bundle;
+package com.liferay.marketplace.util;
 
 import com.liferay.portal.kernel.deploy.auto.AutoDeployException;
 import com.liferay.portal.kernel.log.Log;
