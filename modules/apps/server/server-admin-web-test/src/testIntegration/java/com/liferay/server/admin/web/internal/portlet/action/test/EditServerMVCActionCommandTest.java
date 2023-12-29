2fc280fe75abed6a5def3b30fa37b4331e370088
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2023-12-29 15:42:41 +0100

    LPS-201892 Ensure that values are set correctly (even blank ones) when updating the mail configuration.

--- modules/apps/server/server-admin-web-test/src/testIntegration/java/com/liferay/server/admin/web/internal/portlet/action/test/EditServerMVCActionCommandTest.java
+++ modules/apps/server/server-admin-web-test/src/testIntegration/java/com/liferay/server/admin/web/internal/portlet/action/test/EditServerMVCActionCommandTest.java
@@ -7,7 +7,9 @@ package com.liferay.server.admin.web.internal.portlet.action.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.mail.kernel.model.Account;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutBranch;
@@ -17,24 +19,41 @@ import com.liferay.portal.kernel.model.LayoutTypePortlet;
 import com.liferay.portal.kernel.model.PortletPreferences;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.LayoutBranchLocalService;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.LayoutRevisionLocalService;
 import com.liferay.portal.kernel.service.LayoutSetBranchLocalService;
+import com.liferay.portal.kernel.service.PortalPreferencesLocalService;
 import com.liferay.portal.kernel.service.PortletPreferencesLocalService;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portlet.PortalPreferencesImpl;
+import com.liferay.portlet.PortalPreferencesWrapper;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.BooleanSupplier;
+import java.util.function.IntSupplier;
+import java.util.function.Supplier;
+
+import javax.portlet.ActionRequest;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -52,7 +71,9 @@ public class EditServerMVCActionCommandTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
 
 	@Before
 	public void setUp() throws Exception {
@@ -184,6 +205,34 @@ public class EditServerMVCActionCommandTest {
 				_portletPreferences.getPortletPreferencesId()));
 	}
 
+	@Test
+	public void testUpdateMail() {
+		javax.portlet.PortletPreferences portletPreferences =
+			PrefsPropsUtil.getPreferences(CompanyConstants.SYSTEM);
+
+		try {
+			_testUpdateMailPortletPreferences(
+				RandomTestUtil::randomBoolean, RandomTestUtil::randomInt,
+				RandomTestUtil::randomString, portletPreferences);
+
+			_testUpdateMailPortletPreferences(
+				() -> Boolean.FALSE, () -> 0, () -> StringPool.BLANK,
+				portletPreferences);
+		}
+		finally {
+			PortalPreferencesWrapper portalPreferencesWrapper =
+				(PortalPreferencesWrapper)portletPreferences;
+
+			PortalPreferencesImpl portalPreferencesImpl =
+				portalPreferencesWrapper.getPortalPreferencesImpl();
+
+			_portalPreferencesLocalService.deletePortalPreferences(
+				_portalPreferencesLocalService.fetchPortalPreferences(
+					portalPreferencesImpl.getOwnerId(),
+					portalPreferencesImpl.getOwnerType()));
+		}
+	}
+
 	private PortletPreferences _addPortletPreferences(
 			long ownerId, int ownerType, long plid, String portletId)
 		throws Exception {
@@ -209,6 +258,114 @@ public class EditServerMVCActionCommandTest {
 			layoutBranch.getLayoutBranchId(), _layout.getPlid());
 	}
 
+	private void _testUpdateMailPortletPreferences(
+		BooleanSupplier booleanSupplier, IntSupplier intSupplier,
+		Supplier<String> stringSupplier,
+		javax.portlet.PortletPreferences portletPreferences) {
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		HashMap<String, String> parameters = HashMapBuilder.put(
+			"advancedProperties", stringSupplier.get()
+		).put(
+			"pop3Host", stringSupplier.get()
+		).put(
+			"pop3Password", stringSupplier.get()
+		).put(
+			"pop3Port", String.valueOf(intSupplier.getAsInt())
+		).put(
+			"pop3Secure", stringSupplier.get()
+		).put(
+			"pop3User", stringSupplier.get()
+		).put(
+			"popServerNotificationsEnabled",
+			String.valueOf(booleanSupplier.getAsBoolean())
+		).put(
+			"smtpHost", stringSupplier.get()
+		).put(
+			"smtpPassword", stringSupplier.get()
+		).put(
+			"smtpPort", String.valueOf(intSupplier.getAsInt())
+		).put(
+			"smtpSecure", String.valueOf(booleanSupplier.getAsBoolean())
+		).put(
+			"smtpStartTLSEnable", String.valueOf(booleanSupplier.getAsBoolean())
+		).put(
+			"smtpUser", stringSupplier.get()
+		).build();
+
+		for (Map.Entry<String, String> entry : parameters.entrySet()) {
+			mockLiferayPortletActionRequest.addParameter(
+				entry.getKey(), entry.getValue());
+		}
+
+		ReflectionTestUtil.invoke(
+			_mvcActionCommand, "_updateMail",
+			new Class<?>[] {
+				ActionRequest.class, javax.portlet.PortletPreferences.class
+			},
+			mockLiferayPortletActionRequest, portletPreferences);
+
+		Assert.assertEquals(
+			parameters.get("advancedProperties"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_ADVANCED_PROPERTIES, null));
+		Assert.assertEquals(
+			parameters.get("pop3Host"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_POP3_HOST, null));
+		Assert.assertEquals(
+			parameters.get("pop3Password"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_POP3_PASSWORD, null));
+		Assert.assertEquals(
+			parameters.get("pop3Port"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_POP3_PORT, null));
+		Assert.assertEquals(
+			Boolean.valueOf(parameters.get("pop3Secure")) ?
+				Account.PROTOCOL_POPS : Account.PROTOCOL_POP,
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_STORE_PROTOCOL, null));
+		Assert.assertEquals(
+			parameters.get("pop3User"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_POP3_USER, null));
+		Assert.assertEquals(
+			parameters.get("popServerNotificationsEnabled"),
+			portletPreferences.getValue(
+				PropsKeys.POP_SERVER_NOTIFICATIONS_ENABLED, null));
+		Assert.assertEquals(
+			parameters.get("smtpHost"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_SMTP_HOST, null));
+		Assert.assertEquals(
+			parameters.get("smtpPassword"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_SMTP_PASSWORD, null));
+		Assert.assertEquals(
+			parameters.get("smtpPort"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_SMTP_PORT, null));
+		Assert.assertEquals(
+			Boolean.valueOf(parameters.get("smtpSecure")) ?
+				Account.PROTOCOL_SMTPS : Account.PROTOCOL_SMTP,
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_TRANSPORT_PROTOCOL, null));
+		Assert.assertEquals(
+			parameters.get("smtpStartTLSEnable"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_SMTP_STARTTLS_ENABLE, null));
+		Assert.assertEquals(
+			parameters.get("smtpUser"),
+			portletPreferences.getValue(
+				PropsKeys.MAIL_SESSION_MAIL_SMTP_USER, null));
+	}
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
 	@DeleteAfterTestRun
 	private Group _group;
 
@@ -228,6 +385,12 @@ public class EditServerMVCActionCommandTest {
 
 	@Inject(filter = "mvc.command.name=/server_admin/edit_server")
 	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private Portal _portal;
+
+	@Inject
+	private PortalPreferencesLocalService _portalPreferencesLocalService;
 
 	private PortletPreferences _portletPreferences;
 
