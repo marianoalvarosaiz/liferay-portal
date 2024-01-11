30da03d2cdce0bfbc15319028b3e80f3fc713ea0
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-01-11 11:47:04 +0100

    LPS-198753 Add the white space so we do not get false positives for columns with 'drop' in the name

--- portal-impl/src/com/liferay/portal/db/partition/sql/DBPartitionPostgreSQLDB.java
+++ portal-impl/src/com/liferay/portal/db/partition/sql/DBPartitionPostgreSQLDB.java
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -49,7 +49,7 @@ public class DBPartitionPostgreSQLDB implements DBPartitionDB {
 
 	@Override
 	public String getSafeAlterTable(String alterTableSQL) {
-		if (StringUtil.count(StringUtil.toLowerCase(alterTableSQL), "drop") >
+		if (StringUtil.count(StringUtil.toLowerCase(alterTableSQL), "drop ") >
 				0) {
 
 			return alterTableSQL + " cascade";
