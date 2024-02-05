0515658c03efb4964d0a74b41e6baf8704d0b379
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-02-05 10:38:31 +0100

    LPD-15113 Create test to ensure that when creating an index SQL if the length is used the name is generated taking it into account.

---
+++ modules/util/portal-tools-service-builder/src/test/java/com/liferay/portal/tools/service/builder/IndexMetadataTest.java
@@ -0,0 +1,101 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.tools.service.builder;
+
+import com.liferay.portal.kernel.dao.db.IndexMetadataFactoryUtil;
+import com.liferay.portal.kernel.dao.db.IndexSQLUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.IntegerWrapper;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class IndexMetadataTest {
+
+	@Test
+	public void testIndexNameCanChangeAfterOptimization() throws Exception {
+		IndexMetadata indexMetadata = new IndexMetadata(
+			"Table1", false, "column1", "column2");
+
+		indexMetadata.optimizeColumns(
+			HashMapBuilder.<String, IntegerWrapper>put(
+				"column1", new IntegerWrapper(2)
+			).put(
+				"column2", new IntegerWrapper(1)
+			).build());
+
+		String indexName = (String)ArrayUtil.getValue(
+			StringUtil.split(
+				IndexSQLUtil.getCreateSQL(
+					indexMetadata.getTableName(), indexMetadata.isUnique(),
+					indexMetadata.getColumnNames(),
+					columnNames -> IndexMetadataFactoryUtil.createIndexName(
+						"Table1", columnNames),
+					new int[] {10, 20}),
+				" "),
+			2);
+
+		indexMetadata.optimizeColumns(
+			HashMapBuilder.<String, IntegerWrapper>put(
+				"column1", new IntegerWrapper(1)
+			).put(
+				"column2", new IntegerWrapper(2)
+			).build());
+
+		Assert.assertNotEquals(
+			indexName,
+			(String)ArrayUtil.getValue(
+				StringUtil.split(
+					IndexSQLUtil.getCreateSQL(
+						indexMetadata.getTableName(), indexMetadata.isUnique(),
+						indexMetadata.getColumnNames(),
+						columnNames -> IndexMetadataFactoryUtil.createIndexName(
+							"Table1", columnNames),
+						new int[] {10, 20}),
+					" "),
+				2));
+	}
+
+	@Test
+	public void testIndexNameGenerationCanTakeColumnLengthIntoAccount()
+		throws Exception {
+
+		IndexMetadata indexMetadata = new IndexMetadata(
+			"Company", true, "webId");
+
+		Assert.assertEquals(
+			"IX_EC00543C",
+			(String)ArrayUtil.getValue(
+				StringUtil.split(
+					IndexSQLUtil.getCreateSQL(
+						indexMetadata.getTableName(), indexMetadata.isUnique(),
+						indexMetadata.getColumnNames(),
+						columnNames -> IndexMetadataFactoryUtil.createIndexName(
+							"Company", columnNames),
+						null),
+					" "),
+				3));
+
+		Assert.assertEquals(
+			"IX_B5134427",
+			(String)ArrayUtil.getValue(
+				StringUtil.split(
+					IndexSQLUtil.getCreateSQL(
+						indexMetadata.getTableName(), indexMetadata.isUnique(),
+						indexMetadata.getColumnNames(),
+						columnNames -> IndexMetadataFactoryUtil.createIndexName(
+							"Company", columnNames),
+						new int[] {75}),
+					" "),
+				3));
+	}
+
+}
