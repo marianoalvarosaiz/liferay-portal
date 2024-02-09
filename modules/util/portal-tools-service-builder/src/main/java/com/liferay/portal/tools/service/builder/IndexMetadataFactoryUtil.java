1b8bffbf53ee8555cb411c9371c1a4de4f90892d
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-02-09 13:05:27 +0100

    LPD-15113 Create ServiceBuilder proper IndexMetadata.

---
+++ modules/util/portal-tools-service-builder/src/main/java/com/liferay/portal/tools/service/builder/IndexMetadataFactoryUtil.java
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.tools.service.builder;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class IndexMetadataFactoryUtil {
+
+	public static IndexMetadata createIndexMetadata(
+		boolean unique, String tableName, String... columnNames) {
+
+		if (columnNames == null) {
+			throw new NullPointerException("Column names are missing");
+		}
+
+		return new IndexMetadata(tableName, unique, columnNames);
+	}
+
+	public static IndexMetadata createIndexMetadata(String sql) {
+		com.liferay.portal.kernel.dao.db.IndexMetadata indexMetadata =
+			com.liferay.portal.kernel.dao.db.IndexMetadataFactoryUtil.
+				createIndexMetadata(sql);
+
+		return new IndexMetadata(
+			indexMetadata.getTableName(), indexMetadata.isUnique(),
+			indexMetadata.getColumnNames());
+	}
+
+}
