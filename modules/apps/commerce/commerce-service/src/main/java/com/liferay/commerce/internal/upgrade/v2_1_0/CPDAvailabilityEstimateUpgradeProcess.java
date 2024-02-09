9455fe2c2f4007978f2abe46fb6edfd58c6d7457
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-02-09 13:54:48 +0100

    LPD-15113 Remove cases using the old way. This is no longer needed since indexes are generated automatically and there is no performance issue to justify the creation earlier since we are using primary keys to filter that are already indexed

--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CPDAvailabilityEstimateUpgradeProcess.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CPDAvailabilityEstimateUpgradeProcess.java
@@ -5,25 +5,15 @@
 
 package com.liferay.commerce.internal.upgrade.v2_1_0;
 
-import com.liferay.commerce.model.impl.CPDAvailabilityEstimateModelImpl;
 import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
-import com.liferay.portal.kernel.dao.db.DB;
-import com.liferay.portal.kernel.dao.db.DBManagerUtil;
-import com.liferay.portal.kernel.dao.db.IndexMetadata;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
 import com.liferay.portal.kernel.upgrade.UpgradeStep;
-import com.liferay.portal.kernel.util.ObjectValuePair;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
-import java.util.List;
-import java.util.Objects;
 
 /**
  * @author Alec Sloan
@@ -38,8 +28,6 @@ public class CPDAvailabilityEstimateUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		_addIndexes(CPDAvailabilityEstimateModelImpl.TABLE_NAME);
-
 		try (PreparedStatement preparedStatement = connection.prepareStatement(
 				"update CPDAvailabilityEstimate set CProductId = ? where " +
 					"CPDefinitionId = ?");
@@ -70,57 +58,6 @@ public class CPDAvailabilityEstimateUpgradeProcess extends UpgradeProcess {
 				"CPDAvailabilityEstimate", "CProductId LONG")
 		};
 	}
-
-	private void _addIndexes(String tableName) throws Exception {
-		Class<?> clazz = getClass();
-
-		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
-			clazz.getClassLoader(), tableName);
-
-		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
-			IndexMetadata indexMetadata = indexSQL.getValue();
-
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Adding index %s to table %s",
-						indexMetadata.getIndexName(), tableName));
-			}
-
-			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
-				runSQL(indexMetadata.getCreateSQL(null));
-			}
-			else if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Index %s already exists on table %s",
-						indexMetadata.getIndexName(), tableName));
-			}
-		}
-	}
-
-	private boolean _tableHasIndex(String tableName, String indexName)
-		throws Exception {
-
-		DB db = DBManagerUtil.getDB();
-
-		try (ResultSet resultSet = db.getIndexResultSet(
-				connection, tableName, false)) {
-
-			while (resultSet.next()) {
-				String curIndexName = resultSet.getString("index_name");
-
-				if (Objects.equals(indexName, curIndexName)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CPDAvailabilityEstimateUpgradeProcess.class);
 
 	private final CPDefinitionLocalService _cpDefinitionLocalService;
 
--- modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CommerceSubscriptionEntryUpgradeProcess.java
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CommerceSubscriptionEntryUpgradeProcess.java
@@ -5,27 +5,17 @@
 
 package com.liferay.commerce.internal.upgrade.v2_1_0;
 
-import com.liferay.commerce.model.impl.CommerceSubscriptionEntryModelImpl;
 import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.CPInstanceLocalService;
-import com.liferay.portal.kernel.dao.db.DB;
-import com.liferay.portal.kernel.dao.db.DBManagerUtil;
-import com.liferay.portal.kernel.dao.db.IndexMetadata;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
 import com.liferay.portal.kernel.upgrade.UpgradeStep;
-import com.liferay.portal.kernel.util.ObjectValuePair;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
-import java.util.List;
-import java.util.Objects;
 
 /**
  * @author Ethan Bustad
@@ -42,8 +32,6 @@ public class CommerceSubscriptionEntryUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		_addIndexes(CommerceSubscriptionEntryModelImpl.TABLE_NAME);
-
 		try (PreparedStatement preparedStatement = connection.prepareStatement(
 				"update CommerceSubscriptionEntry set CProductId = ?," +
 					"CPInstanceUUID = ? where CPInstanceId = ?");
@@ -88,57 +76,6 @@ public class CommerceSubscriptionEntryUpgradeProcess extends UpgradeProcess {
 				"CProductId LONG")
 		};
 	}
-
-	private void _addIndexes(String tableName) throws Exception {
-		Class<?> clazz = getClass();
-
-		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
-			clazz.getClassLoader(), tableName);
-
-		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
-			IndexMetadata indexMetadata = indexSQL.getValue();
-
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Adding index %s to table %s",
-						indexMetadata.getIndexName(), tableName));
-			}
-
-			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
-				runSQL(indexMetadata.getCreateSQL(null));
-			}
-			else if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Index %s already exists on table %s",
-						indexMetadata.getIndexName(), tableName));
-			}
-		}
-	}
-
-	private boolean _tableHasIndex(String tableName, String indexName)
-		throws Exception {
-
-		DB db = DBManagerUtil.getDB();
-
-		try (ResultSet resultSet = db.getIndexResultSet(
-				connection, tableName, false)) {
-
-			while (resultSet.next()) {
-				String curIndexName = resultSet.getString("index_name");
-
-				if (Objects.equals(indexName, curIndexName)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CommerceSubscriptionEntryUpgradeProcess.class);
 
 	private final CPDefinitionLocalService _cpDefinitionLocalService;
 	private final CPInstanceLocalService _cpInstanceLocalService;
