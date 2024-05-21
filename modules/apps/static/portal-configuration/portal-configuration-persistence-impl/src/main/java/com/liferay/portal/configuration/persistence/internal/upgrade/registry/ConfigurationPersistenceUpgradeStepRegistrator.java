e2304d341c2a8d6e02fbad49a7af812ec6a563ef
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-05-21 08:32:57 +0200

    LPD-25784 Tests.

--- modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/upgrade/registry/ConfigurationPersistenceUpgradeStepRegistrator.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-impl/src/main/java/com/liferay/portal/configuration/persistence/internal/upgrade/registry/ConfigurationPersistenceUpgradeStepRegistrator.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.configuration.persistence.internal.upgrade.registry;
 
-import com.liferay.portal.configuration.persistence.internal.upgrade.schema.SchemaCreationUpgradeStep;
 import com.liferay.portal.configuration.persistence.internal.upgrade.v2_0_0.ConfigurationUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcessFactory;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
@@ -24,9 +23,6 @@ public class ConfigurationPersistenceUpgradeStepRegistrator
 	@Override
 	public void register(Registry registry) {
 		registry.registerInitialization();
-
-		registry.registerReleaseCreationUpgradeSteps(
-			new SchemaCreationUpgradeStep());
 
 		registry.register(
 			"0.0.1", "0.0.2",
