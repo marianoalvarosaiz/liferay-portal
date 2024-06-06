65d2337c64a37df31fa004ccd0b99aac5c0a635f
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-06-06 16:19:45 +0200

    LPD-25784 Create infrastructure to be able to record SQL based on the database type, but without persisting it to the database.

---
+++ modules/apps/portal/portal-db-schema-info/src/main/java/com/liferay/portal/db/schema/info/internal/sql/FakeDBFactory.java
@@ -0,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.db.schema.info.internal.sql;
+
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBFactory;
+import com.liferay.portal.kernel.dao.db.DBType;
+
+import java.util.ServiceLoader;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class FakeDBFactory {
+
+	public static DB getDB(DBType dbType) {
+		ServiceLoader<DBFactory> serviceLoader = ServiceLoader.load(
+			DBFactory.class, DBFactory.class.getClassLoader());
+
+		for (DBFactory dbFactory : serviceLoader) {
+			if (dbFactory.getDBType() == dbType) {
+				return dbFactory.create(0, 0);
+			}
+		}
+
+		throw new IllegalArgumentException("Illegal database type " + dbType);
+	}
+
+}
