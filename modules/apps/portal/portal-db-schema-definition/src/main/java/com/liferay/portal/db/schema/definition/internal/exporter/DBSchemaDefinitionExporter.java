0d0ed9a844c98761d31bbd248bad995d09d2eaa7
Alberto Chaparro <alberto.chaparro@liferay.com>
2024-06-11 17:18:30 +0200

    LPD-25784 Naming

---
+++ modules/apps/portal/portal-db-schema-definition/src/main/java/com/liferay/portal/db/schema/definition/internal/exporter/DBSchemaDefinitionExporter.java
@@ -0,0 +1,133 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.db.schema.definition.internal.exporter;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.db.schema.definition.internal.configuration.DBSchemaDefinitionExporterConfiguration;
+import com.liferay.portal.db.schema.definition.internal.processor.SQLFilesProcessor;
+import com.liferay.portal.db.schema.definition.internal.recorder.SQLRecorder;
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.util.PropsValues;
+
+import java.io.File;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Map;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+@Component(
+	configurationPid = "com.liferay.portal.db.schema.definition.internal.configuration.DBSchemaDefinitionExporterConfiguration",
+	configurationPolicy = ConfigurationPolicy.REQUIRE, service = {}
+)
+public class DBSchemaDefinitionExporter {
+
+	@Activate
+	protected void activate(Map<String, Object> properties) {
+		_exportDBSchemaDefinition(properties);
+	}
+
+	private void _deleteConfiguration(String pid) {
+		try {
+			Path path = Paths.get(
+				PropsValues.MODULE_FRAMEWORK_CONFIGS_DIR,
+				pid.concat(".config"));
+
+			if (Files.exists(path)) {
+				Files.delete(path);
+			}
+			else {
+				Configuration[] configurations =
+					_configurationAdmin.listConfigurations(
+						StringBundler.concat(
+							"(", Constants.SERVICE_PID, StringPool.EQUAL, pid,
+							"*)"));
+
+				if (configurations == null) {
+					return;
+				}
+
+				for (Configuration configuration : configurations) {
+					configuration.delete();
+				}
+			}
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
+	}
+
+	private void _exportDBSchemaDefinition(Map<String, Object> properties) {
+		if (_log.isInfoEnabled()) {
+			_log.info("Database schema definition export started");
+		}
+
+		try {
+			DBSchemaDefinitionExporterConfiguration
+				dbSchemaDefinitionExporterConfiguration =
+					ConfigurableUtil.createConfigurable(
+						DBSchemaDefinitionExporterConfiguration.class,
+						properties);
+
+			SQLRecorder sqlRecorder = new SQLRecorder();
+
+			new SQLFilesProcessor(
+				DBType.valueOf(
+					StringUtil.toUpperCase(
+						dbSchemaDefinitionExporterConfiguration.
+							databaseType())),
+				sqlRecorder
+			).process();
+
+			FileUtil.write(
+				new File(
+					dbSchemaDefinitionExporterConfiguration.path(),
+					"indexes.sql"),
+				sqlRecorder.getIndexesSQL());
+			FileUtil.write(
+				new File(
+					dbSchemaDefinitionExporterConfiguration.path(),
+					"tables.sql"),
+				sqlRecorder.getTablesSQL());
+
+			if (_log.isInfoEnabled()) {
+				_log.info("Database schema definition export finished");
+			}
+		}
+		catch (Exception exception) {
+			_log.error(
+				"Unable to export database schema definition", exception);
+		}
+		finally {
+			_deleteConfiguration((String)properties.get("service.pid"));
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DBSchemaDefinitionExporter.class);
+
+	@Reference
+	private ConfigurationAdmin _configurationAdmin;
+
+}
---
+++ modules/apps/portal/portal-db-schema-definition/src/main/java/com/liferay/portal/db/schema/definition/internal/recorder/SQLRecorder.java
@@ -0,0 +1,59 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.db.schema.definition.internal.recorder;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.StringUtil;
+
+/**
+ * @author Mariano Álvaro Sáiz
+ */
+public class SQLRecorder {
+
+	public String getIndexesSQL() {
+		return _indexesSB.toString();
+	}
+
+	public String getTablesSQL() {
+		return _tablesSB.toString();
+	}
+
+	public void recordIndexesSQL(String sql) {
+		if (sql != null) {
+			_indexesSB.append(sql);
+		}
+	}
+
+	public void recordSQL(String sql) {
+		if (sql == null) {
+			return;
+		}
+
+		sql += StringPool.NEW_LINE;
+
+		String lowerCaseSQL = StringUtil.toLowerCase(sql);
+
+		if (lowerCaseSQL.contains("create index") ||
+			lowerCaseSQL.contains("create unique index")) {
+
+			_indexesSB.append(sql);
+		}
+		else {
+			_tablesSB.append(sql);
+		}
+	}
+
+	public void recordTablesSQL(String sql) {
+		if (sql != null) {
+			_tablesSB.append(sql);
+		}
+	}
+
+	private final StringBundler _indexesSB = new StringBundler();
+	private final StringBundler _tablesSB = new StringBundler();
+
+}
