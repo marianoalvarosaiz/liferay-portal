a93ec28fe19d032c9063f881f3291223b2efbe98
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-06-11 17:10:39 +0200

    LPD-25786 Generate SQL for Objects tables and indexes also.

--- modules/apps/portal/portal-db-schema-info/src/main/java/com/liferay/portal/db/schema/info/internal/processor/DBSchemaToFilesProcessor.java
+++ modules/apps/portal/portal-db-schema-info/src/main/java/com/liferay/portal/db/schema/info/internal/processor/DBSchemaToFilesProcessor.java
@@ -5,6 +5,9 @@
 
 package com.liferay.portal.db.schema.info.internal.processor;
 
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.portal.db.schema.info.internal.sql.SQLRecorder;
 import com.liferay.portal.kernel.dao.db.DBType;
 import com.liferay.portal.kernel.util.FileUtil;
@@ -16,29 +19,47 @@ import java.io.File;
  */
 public class DBSchemaToFilesProcessor {
 
-	public DBSchemaToFilesProcessor(DBType dbType) {
+	public DBSchemaToFilesProcessor(
+		DBType dbType,
+		ObjectDefinitionLocalService objectDefinitionLocalService,
+		ObjectFieldLocalService objectFieldLocalService,
+		ObjectRelationshipLocalService objectRelationshipLocalService) {
+
 		_dbType = dbType;
+		_objectDefinitionLocalService = objectDefinitionLocalService;
+		_objectFieldLocalService = objectFieldLocalService;
+		_objectRelationshipLocalService = objectRelationshipLocalService;
 	}
 
 	public void processTo(String path) throws Exception {
+		SQLRecorder sqlRecorder = new SQLRecorder();
+
 		new DBSchemaToSQLProcessor(
-			_dbType, _sqlRecorder
+			_dbType, sqlRecorder
+		).process();
+
+		new DBObjectsSchemaToSQLProcessor(
+			_dbType, _objectDefinitionLocalService, _objectFieldLocalService,
+			_objectRelationshipLocalService, sqlRecorder
 		).process();
 
 		FileUtil.write(
 			new File(path, "indexes" + _DEFAULT_EXTENSION),
-			_sqlRecorder.getIndexesSQL());
+			sqlRecorder.getIndexesSQL());
 		FileUtil.write(
 			new File(path, "sequences" + _DEFAULT_EXTENSION),
-			_sqlRecorder.getSequencesSQL());
+			sqlRecorder.getSequencesSQL());
 		FileUtil.write(
 			new File(path, "tables" + _DEFAULT_EXTENSION),
-			_sqlRecorder.getTablesSQL());
+			sqlRecorder.getTablesSQL());
 	}
 
 	private static final String _DEFAULT_EXTENSION = ".sql";
 
 	private final DBType _dbType;
-	private final SQLRecorder _sqlRecorder = new SQLRecorder();
+	private final ObjectDefinitionLocalService _objectDefinitionLocalService;
+	private final ObjectFieldLocalService _objectFieldLocalService;
+	private final ObjectRelationshipLocalService
+		_objectRelationshipLocalService;
 
 }
