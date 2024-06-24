69ec50f8f43a8b6a9b6b55b5003e8343139e0743
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-06-24 21:13:50 +0200

    Revert "LPD-25786 Use the same pattern than in other DynamicObjects to avoid having the create tables sentences spread accross the code."
    This reverts commit c6b699567e101d8370a966dc9078485b3af8f868.

--- modules/apps/object/object-api/src/main/java/com/liferay/object/petra/sql/dsl/DynamicObjectRelationshipMappingTable.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/petra/sql/dsl/DynamicObjectRelationshipMappingTable.java
@@ -7,9 +7,6 @@ package com.liferay.object.petra.sql.dsl;
 
 import com.liferay.petra.sql.dsl.Column;
 import com.liferay.petra.sql.dsl.base.BaseTable;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 
 import java.sql.Types;
 
@@ -27,7 +24,6 @@ public class DynamicObjectRelationshipMappingTable
 
 		_primaryKeyColumnName1 = primaryKeyColumnName1;
 		_primaryKeyColumnName2 = primaryKeyColumnName2;
-		_tableName = tableName;
 
 		createColumn(
 			_primaryKeyColumnName1, Long.class, Types.BIGINT,
@@ -35,20 +31,6 @@ public class DynamicObjectRelationshipMappingTable
 		createColumn(
 			_primaryKeyColumnName2, Long.class, Types.BIGINT,
 			Column.FLAG_PRIMARY);
-	}
-
-	public String getCreateTableSQL() {
-		String sql = StringBundler.concat(
-			"create table ", _tableName, " (", _primaryKeyColumnName1,
-			" LONG not null,", _primaryKeyColumnName2,
-			" LONG not null, primary key (", _primaryKeyColumnName1, ", ",
-			_primaryKeyColumnName2, "));");
-
-		if (_log.isDebugEnabled()) {
-			_log.debug("SQL: " + sql);
-		}
-
-		return sql;
 	}
 
 	public Column<DynamicObjectRelationshipMappingTable, Long>
@@ -65,11 +47,7 @@ public class DynamicObjectRelationshipMappingTable
 			_primaryKeyColumnName2);
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		DynamicObjectRelationshipMappingTable.class);
-
 	private final String _primaryKeyColumnName1;
 	private final String _primaryKeyColumnName2;
-	private final String _tableName;
 
 }
