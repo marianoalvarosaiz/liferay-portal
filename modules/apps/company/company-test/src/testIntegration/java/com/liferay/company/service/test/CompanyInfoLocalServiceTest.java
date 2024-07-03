9d98bcbfa10d7bc982bfbee8aee9894c85afe98b
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-07-03 10:29:38 +0200

    LPD-30211 Perform proper testing. Since partition and cache is removed it doesn't make sense to search in the CompanyThreadLocal scope of the deleted company. What must be done is to check that the sharded related caches have been removed.

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyInfoLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyInfoLocalServiceTest.java
@@ -8,8 +8,17 @@ package com.liferay.company.service.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.cache.PortalCacheException;
+import com.liferay.portal.kernel.cache.PortalCacheManager;
+import com.liferay.portal.kernel.cache.PortalCacheManagerListener;
+import com.liferay.portal.kernel.cache.PortalCacheManagerNames;
+import com.liferay.portal.kernel.cache.PortalCacheManagerProvider;
+import com.liferay.portal.kernel.dao.orm.EntityCache;
+import com.liferay.portal.kernel.dao.orm.FinderCache;
 import com.liferay.portal.kernel.encryptor.Encryptor;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.model.CompanyInfo;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.CompanyInfoLocalService;
@@ -18,6 +27,8 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.model.impl.CompanyInfoImpl;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -51,11 +62,44 @@ public class CompanyInfoLocalServiceTest {
 	public void testDeleteCompanyInfo() throws Exception {
 		long companyId = _company.getCompanyId();
 
-		_companyLocalService.deleteCompany(_company);
+		CompanyInfo companyInfo = null;
+
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setWithSafeCloseable(companyId)) {
+
+			companyInfo = _companyInfoLocalService.fetchCompany(companyId);
+		}
+
+		PortalCacheManagerListener portalCacheManagerListener =
+			new CacheRemovedPortalCacheManagerListener();
+
+		_portalCacheManager.registerPortalCacheManagerListener(
+			portalCacheManagerListener);
+
+		_entityCacheRemoved = false;
+		_finderCacheRemoved = false;
+
+		try {
+			_companyLocalService.deleteCompany(_company);
+		}
+		finally {
+			_portalCacheManager.unregisterPortalCacheManagerListener(
+				portalCacheManagerListener);
+		}
 
 		_company = null;
 
-		Assert.assertNull(_companyInfoLocalService.fetchCompany(companyId));
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setWithSafeCloseable(
+					CompanyConstants.SYSTEM)) {
+
+			Assert.assertNull(
+				_companyInfoLocalService.fetchCompanyInfo(
+					companyInfo.getCompanyInfoId()));
+		}
+
+		Assert.assertTrue(_entityCacheRemoved);
+		Assert.assertTrue(_finderCacheRemoved);
 	}
 
 	@Test
@@ -122,6 +166,17 @@ public class CompanyInfoLocalServiceTest {
 		}
 	}
 
+	private static final String _ENTITY_CACHE_PREFIX =
+		EntityCache.class.getName() + StringPool.PERIOD +
+			CompanyInfoImpl.class.getName();
+
+	private static final String _FINDER_CACHE_PREFIX =
+		FinderCache.class.getName() + StringPool.PERIOD +
+			CompanyInfoImpl.class.getName();
+
+	@DeleteAfterTestRun
+	private static Company _company;
+
 	@Inject
 	private static CompanyInfoLocalService _companyInfoLocalService;
 
@@ -134,7 +189,45 @@ public class CompanyInfoLocalServiceTest {
 	@Inject
 	private static Encryptor _encryptor;
 
-	@DeleteAfterTestRun
-	private Company _company;
+	private static boolean _entityCacheRemoved;
+	private static boolean _finderCacheRemoved;
+	private static final PortalCacheManager<?, ?> _portalCacheManager =
+		PortalCacheManagerProvider.getPortalCacheManager(
+			PortalCacheManagerNames.MULTI_VM);
+
+	private static class CacheRemovedPortalCacheManagerListener
+		implements PortalCacheManagerListener {
+
+		@Override
+		public void dispose() throws PortalCacheException {
+		}
+
+		@Override
+		public void init() throws PortalCacheException {
+		}
+
+		@Override
+		public void notifyPortalCacheAdded(String portalCacheName) {
+		}
+
+		@Override
+		public void notifyPortalCacheRemoved(String portalCacheName) {
+			if (!StringUtil.endsWith(
+					portalCacheName, String.valueOf(_company.getCompanyId()))) {
+
+				return;
+			}
+
+			if (StringUtil.startsWith(portalCacheName, _ENTITY_CACHE_PREFIX)) {
+				_entityCacheRemoved = true;
+			}
+			else if (StringUtil.startsWith(
+						portalCacheName, _FINDER_CACHE_PREFIX)) {
+
+				_finderCacheRemoved = true;
+			}
+		}
+
+	}
 
 }
