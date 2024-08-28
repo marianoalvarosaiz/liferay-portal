cf83779fc90756362f9dba02cccc5a8da5139cb3
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-08-28 09:52:34 +0200

    Revert "LPD-34471 Move"
    This reverts commit 8aecdeea188c7659808f4893f1adab1ebbbe48d9.

--- portal-impl/src/com/liferay/portal/util/PortalInstances.java
+++ portal-impl/src/com/liferay/portal/util/PortalInstances.java
@@ -33,9 +33,9 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.SiteInitializerThreadLocal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.site.initializer.kernel.util.SiteInitializerThreadLocal;
 
 import java.sql.SQLException;
 
