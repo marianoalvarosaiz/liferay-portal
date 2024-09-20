b5240ef03feedde0a8c08c7b5e157e8167d0704e
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2024-09-20 20:22:18 +0200

    LPD-30986 Redesign test. It is not enough to create a fake partition since the search index will also be needed.

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceDBPartitionTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectDefinitionLocalServiceDBPartitionTest.java
@@ -11,19 +11,27 @@ import com.liferay.object.field.builder.TextObjectFieldBuilder;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.test.util.ObjectDefinitionTestUtil;
 import com.liferay.petra.lang.SafeCloseable;
-import com.liferay.portal.db.partition.test.util.BaseDBPartitionTestCase;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.db.partition.DBPartition;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.AssumeTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.Collections;
 
-import org.junit.AfterClass;
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Assume;
+import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -31,23 +39,27 @@ import org.junit.runner.RunWith;
  * @author Carolina Barbosa
  */
 @RunWith(Arquillian.class)
-public class ObjectDefinitionLocalServiceDBPartitionTest
-	extends BaseDBPartitionTestCase {
+public class ObjectDefinitionLocalServiceDBPartitionTest {
 
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-		BaseDBPartitionTestCase.setUpClass();
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new AssumeTestRule("assume"), new LiferayIntegrationTestRule());
 
-		BaseDBPartitionTestCase.setUpDBPartitions();
-	}
+	public static void assume() {
+		DBType dbType = DBManagerUtil.getDBType();
 
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-		BaseDBPartitionTestCase.tearDownDBPartitions();
+		Assume.assumeTrue(
+			(dbType == DBType.MYSQL) || (dbType == DBType.POSTGRESQL));
+
+		Assume.assumeTrue(DBPartition.isPartitionEnabled());
 	}
 
 	@Test
 	public void testPublishObjectDefinition() throws Exception {
+		_company = CompanyTestUtil.addCompany();
+
 		_objectDefinition = null;
 
 		try (SafeCloseable safeCloseable =
@@ -69,8 +81,8 @@ public class ObjectDefinitionLocalServiceDBPartitionTest
 
 		_assertResourceActionsCount(
 			TestPropsValues.getCompanyId(), _objectDefinition, 4);
-		_assertResourceActionsCount(COMPANY_IDS[0], _objectDefinition, 0);
-		_assertResourceActionsCount(COMPANY_IDS[1], _objectDefinition, 0);
+		_assertResourceActionsCount(
+			_company.getCompanyId(), _objectDefinition, 0);
 	}
 
 	private void _assertResourceActionsCount(
@@ -86,6 +98,9 @@ public class ObjectDefinitionLocalServiceDBPartitionTest
 					objectDefinition.getClassName()));
 		}
 	}
+
+	@DeleteAfterTestRun
+	private Company _company;
 
 	@DeleteAfterTestRun
 	private ObjectDefinition _objectDefinition;
