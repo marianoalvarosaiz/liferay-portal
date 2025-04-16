5120759c5756bd67a83d95eb10583ae37039fcb3
IstvanD <istvan.dezsi@liferay.com>
2025-04-16 11:06:57 +0200

    LPD-6627 Surrounding method.invoke with try-catch as that is the origin of the exception when a connection cannot be established.
    Moving the warning message.

--- portal-kernel/src/com/liferay/portal/kernel/dao/db/BaseDBProcess.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/db/BaseDBProcess.java
@@ -37,6 +37,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import java.sql.Connection;
@@ -603,27 +604,6 @@ public abstract class BaseDBProcess implements DBProcess {
 			return DataAccess.getConnection();
 		}
 		catch (Exception exception) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					"Unable to get connection for company " +
-						CompanyThreadLocal.getCompanyId());
-
-				int connectionsCount = _countConnections();
-
-				int maximumPoolSize = GetterUtil.getInteger(
-					PropsUtil.get("jdbc.default.maximumPoolSize"), 180);
-
-				if (connectionsCount >= (maximumPoolSize * 0.8)) {
-					_log.warn(
-						StringBundler.concat(
-							"Number of open connections (", connectionsCount,
-							") is near the connection pool limit (",
-							maximumPoolSize, "). Consider increasing ",
-							"\"jdbc.default.maximumPoolSize\" to improve ",
-							"performance."));
-				}
-			}
-
 			return ReflectionUtil.throwException(exception);
 		}
 	}
@@ -787,10 +767,46 @@ public abstract class BaseDBProcess implements DBProcess {
 					CompanyThreadLocal.getCompanyId(),
 					key -> new ConcurrentHashMap<>());
 
-			return method.invoke(
-				connectionMap.computeIfAbsent(
-					Thread.currentThread(), thread -> _getConnection()),
-				args);
+			Object invocation = null;
+
+			try {
+				invocation = method.invoke(
+					connectionMap.computeIfAbsent(
+						Thread.currentThread(), thread -> _getConnection()),
+					args);
+			}
+			catch (InvocationTargetException invocationTargetException) {
+				Throwable targetThrowable =
+					invocationTargetException.getTargetException();
+
+				if (targetThrowable instanceof SQLException) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"Unable to get connection for company " +
+								CompanyThreadLocal.getCompanyId());
+
+						int connectionsCount = _countConnections();
+
+						int maximumPoolSize = GetterUtil.getInteger(
+							PropsUtil.get("jdbc.default.maximumPoolSize"), 180);
+
+						if (connectionsCount >= (maximumPoolSize - 10)) {
+							_log.warn(
+								StringBundler.concat(
+									"Number of open connections (",
+									connectionsCount,
+									") is near the connection pool limit (",
+									maximumPoolSize, "). Consider increasing ",
+									"\"jdbc.default.maximumPoolSize\" to ",
+									"improve performance."));
+						}
+					}
+				}
+
+				throw invocationTargetException;
+			}
+
+			return invocation;
 		}
 
 	}
