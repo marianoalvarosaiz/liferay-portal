61f10a82d5f4c31910383aa86213edc0197f9886
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-05-31 08:48:01 +0200

    LPD-56479 Revert 224390dea591f7c5f6a348dd87a8d10651ddf6d4. UpgradeProcess don't run in parallel but VerifyProcessSuite is a VerifyProcess that contains verify processes. In this case they can run in parallel.

--- portal-kernel/src/com/liferay/portal/kernel/dao/db/BaseDBProcess.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/db/BaseDBProcess.java
@@ -799,7 +799,7 @@ public abstract class BaseDBProcess implements DBProcess {
 
 	private static final Log _log = LogFactoryUtil.getLog(BaseDBProcess.class);
 
-	private static final Map<Long, Map<Thread, Connection>> _connectionsMaps =
+	private final Map<Long, Map<Thread, Connection>> _connectionsMaps =
 		new ConcurrentHashMap<>();
 	private static final AtomicInteger _fixedThreadPoolSize = new AtomicInteger(
 		0);
