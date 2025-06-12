fc7b9a9ad024911a65d87e52dae725ea042d2ed5
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-12 17:16:15 +0200

    LPD-56347 Register version display name when creating portal release.

--- portal-impl/src/com/liferay/portal/upgrade/PortalUpgradeProcess.java
+++ portal-impl/src/com/liferay/portal/upgrade/PortalUpgradeProcess.java
@@ -50,9 +50,9 @@ public class PortalUpgradeProcess extends UpgradeProcess {
 				StringBundler.concat(
 					"insert into Release_ (releaseId, createDate, ",
 					"modifiedDate, servletContextName, schemaVersion, ",
-					"buildNumber, buildDate, verified, testString) values (",
-					ReleaseConstants.DEFAULT_ID,
-					", ?, ?, ?, ?, ?, ?, ?, ?)"))) {
+					"buildNumber, buildDate, versionName, verified, ",
+					"testString) values (", ReleaseConstants.DEFAULT_ID,
+					", ?, ?, ?, ?, ?, ?, ?, ?, ?)"))) {
 
 			Date date = new Date(System.currentTimeMillis());
 
@@ -72,8 +72,9 @@ public class PortalUpgradeProcess extends UpgradeProcess {
 
 			preparedStatement.setDate(6, new Date(buildDate.getTime()));
 
-			preparedStatement.setBoolean(7, false);
-			preparedStatement.setString(8, ReleaseConstants.TEST_STRING);
+			preparedStatement.setString(7, ReleaseInfo.getVersionDisplayName());
+			preparedStatement.setBoolean(8, false);
+			preparedStatement.setString(9, ReleaseConstants.TEST_STRING);
 
 			preparedStatement.executeUpdate();
 
