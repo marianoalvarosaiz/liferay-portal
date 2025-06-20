72083c47b094e2bb247e178a23178454d4da6cd3
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 15:21:56 +0200

    Revert "LPD-46121 buildREST"
    This reverts commit 4210274f676393160b173da0388e8b079777d878.

--- modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/BaseAssetLibraryTestEntityResourceTestCase.java
+++ modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/BaseAssetLibraryTestEntityResourceTestCase.java
@@ -109,6 +109,7 @@ public abstract class BaseAssetLibraryTestEntityResourceTestCase {
 				}
 			});
 		irrelevantDepotEntryGroup = irrelevantDepotEntry.getGroup();
+
 		testDepotEntry = DepotEntryLocalServiceUtil.addDepotEntry(
 			Collections.singletonMap(
 				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
--- modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/BaseERCScopedTestEntityResourceTestCase.java
+++ modules/util/portal-tools-rest-builder-test-test/src/testIntegration/java/com/liferay/portal/tools/rest/builder/test/resource/v1_0/test/BaseERCScopedTestEntityResourceTestCase.java
@@ -112,6 +112,7 @@ public abstract class BaseERCScopedTestEntityResourceTestCase {
 				}
 			});
 		irrelevantDepotEntryGroup = irrelevantDepotEntry.getGroup();
+
 		testDepotEntry = DepotEntryLocalServiceUtil.addDepotEntry(
 			Collections.singletonMap(
 				LocaleUtil.getDefault(), RandomTestUtil.randomString()),
@@ -1520,7 +1521,6 @@ public abstract class BaseERCScopedTestEntityResourceTestCase {
 		randomIrrelevantERCScopedTestEntity.
 			setAssetLibraryExternalReferenceCode(
 				irrelevantDepotEntryGroup.getExternalReferenceCode());
-
 		randomIrrelevantERCScopedTestEntity.setSiteExternalReferenceCode(
 			irrelevantGroup.getExternalReferenceCode());
 
