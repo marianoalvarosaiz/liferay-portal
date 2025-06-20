237e27a0af07fcdb15747abe5ec8c2fa3bee9238
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 15:19:54 +0200

    Revert "LPD-56418 Add delete helper to ApiHelper"
    This reverts commit 8e2eb72e5ae336513ad917d8b77cab3b514def32.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/services/ApiHelper.ts
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/services/ApiHelper.ts
@@ -32,15 +32,6 @@ type RequestResult<T> =
 			error: null;
 	  };
 
-async function deleteRequest(url: string) {
-	return handleRequest(() =>
-		fetch(url, {
-			headers: HEADERS,
-			method: 'DELETE',
-		})
-	);
-}
-
 async function handleRequest<T>(
 	fetcher: () => Promise<Response>
 ): Promise<RequestResult<T>> {
@@ -129,4 +120,4 @@ async function patch<T>(data: any, url: string) {
 	);
 }
 
-export default {delete: deleteRequest, get, patch, post, postFormData, put};
+export default {get, patch, post, postFormData, put};
