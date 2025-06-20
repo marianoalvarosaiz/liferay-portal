1f6747b34b58c0e2a4d12b1ebae979c9cdef2493
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:54:46 +0200

    Revert "LPD-58050 make InventoryAnalysisCard filters responsive"
    This reverts commit 2db356983477af2eb28ffc92131d8ff4a76cee42.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/dashboard/components/InventoryAnalysisCard.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/dashboard/components/InventoryAnalysisCard.tsx
@@ -160,60 +160,41 @@ export function InventoryAnalysisCard() {
 				)}
 				title={Liferay.Language.get('inventory-analysis')}
 			>
-				<div className="align-items-lg-center d-flex flex-column flex-lg-row">
-					<div className="align-items-center d-flex mb-2 mb-md-0 mr-md-4">
-						<span className="mr-2">
-							<Text size={3} weight="semi-bold">
-								{Liferay.Language.get('group-by')}
-							</Text>
-						</span>
+				<div className="align-items-center d-flex">
+					<span className="ml-1 mr-2">
+						<Text size={3} weight="semi-bold">
+							{Liferay.Language.get('group-by')}
+						</Text>
+					</span>
 
-						<GroupByDropdown
-							item={structureType}
-							onSelectItem={setStructureType}
-							setStructureTypeData={setStructureTypeData}
-						/>
-					</div>
+					<GroupByDropdown
+						item={structureType}
+						onSelectItem={setStructureType}
+						setStructureTypeData={setStructureTypeData}
+					/>
 
-					<div className="d-flex flex-md-row flex-row flex-xs-column">
-						<div className="align-items-center d-flex mb-2 mb-lg-0 mr-lg-3">
-							<span className="align-self-lg-auto align-self-start mr-2">
-								<Text size={3} weight="semi-bold">
-									{Liferay.Language.get('filter-by')}
-								</Text>
-							</span>
-						</div>
+					<span className="ml-3 mr-2">
+						<Text size={3} weight="semi-bold">
+							{Liferay.Language.get('filter-by')}
+						</Text>
+					</span>
 
-						<div className="d-flex flex-wrap">
-							<div className="mb-2 mb-lg-0 mr-2">
-								<AllStructureTypesDropdown
-									item={structure}
-									onSelectItem={setStructure}
-								/>
-							</div>
+					<AllStructureTypesDropdown
+						item={structure}
+						onSelectItem={setStructure}
+					/>
 
-							<div className="mb-2 mb-lg-0 mr-2">
-								<AllVocabulariesDropdown
-									item={vocabulary}
-									onSelectItem={setVocabulary}
-								/>
-							</div>
+					<AllVocabulariesDropdown
+						item={vocabulary}
+						onSelectItem={setVocabulary}
+					/>
 
-							<div className="mb-2 mb-lg-0 mr-2">
-								<AllCategoriesDropdown
-									item={category}
-									onSelectItem={setCategory}
-								/>
-							</div>
+					<AllCategoriesDropdown
+						item={category}
+						onSelectItem={setCategory}
+					/>
 
-							<div className="mb-2 mb-lg-0">
-								<AllTagsDropdown
-									item={tag}
-									onSelectItem={setTag}
-								/>
-							</div>
-						</div>
-					</div>
+					<AllTagsDropdown item={tag} onSelectItem={setTag} />
 				</div>
 
 				<Table
