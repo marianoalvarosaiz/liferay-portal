1670e445a5404f6dfcc490ae717175c97330da4d
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:56:03 +0200

    Revert "LPD-57356 SF formatting"
    This reverts commit 077f1b3111a6ddfc628c7697cc996a3ee0a43c9f.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -4871,8 +4871,7 @@ public class ObjectEntryLocalServiceImpl
 		values.put(columnName + "i18n", (Serializable)localizedValues);
 		values.putIfAbsent(
 			StringUtil.removeLast(columnName, StringPool.UNDERLINE),
-			GetterUtil.get(
-				localizedValues.get(defaultLanguageId), StringPool.BLANK));
+			GetterUtil.get(localizedValues.get(defaultLanguageId), StringPool.BLANK));
 	}
 
 	private void _putObjectFilterParser(
