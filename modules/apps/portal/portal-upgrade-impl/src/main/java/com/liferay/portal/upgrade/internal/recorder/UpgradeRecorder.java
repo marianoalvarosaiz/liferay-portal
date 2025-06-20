c2a9cc3906c2fdde69d98bcc59375842a3daf0c8
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:52:43 +0200

    Revert "LPD-53153 exception must be processed where thrown"
    This reverts commit e4c6cc8e02a15196777f814481a1abdd89e3740c.

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/recorder/UpgradeRecorder.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/recorder/UpgradeRecorder.java
@@ -165,9 +165,13 @@ public class UpgradeRecorder {
 				_log.info("No pending upgrades to run");
 			}
 			else {
-				if (!_errorMessages.containsKey(
-						PreupgradeVerifyProcessSuite.class.getName())) {
-
+				if (_errorMessages.containsKey(
+					PreupgradeVerifyProcessSuite.class.getName())) {
+					_log.info(
+						"A preupgrade verification process has failed. " +
+						"Please fix the reported issues and re-run the upgrade.");
+				}
+				else{
 					_log.info(
 						StringBundler.concat(
 							StringUtil.upperCaseFirstLetter(_type),
@@ -175,10 +179,11 @@ public class UpgradeRecorder {
 
 					if (!_result.equals("failure") &&
 						!_errorMessages.isEmpty()) {
-
 						_log.info("Unrelated errors occur during the upgrade");
 					}
 				}
+
+
 			}
 		}
 
