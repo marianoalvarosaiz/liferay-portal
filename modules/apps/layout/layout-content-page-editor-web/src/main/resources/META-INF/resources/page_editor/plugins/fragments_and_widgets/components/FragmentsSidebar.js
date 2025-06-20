44c1807cd5bb6bacffdee98a3b6dc78b3104c6a2
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:58:38 +0200

    Revert "LPD-57726 Add a common div parent with flex-shrink-0"
    This reverts commit c11ab106e2cc93bcc812dcd338f54695b6b71311.

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments_and_widgets/components/FragmentsSidebar.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/fragments_and_widgets/components/FragmentsSidebar.js
@@ -243,79 +243,78 @@ export default function FragmentsSidebar() {
 						onChange={setSearchValue}
 					/>
 
-					<div className="d-flex flex-shrink-0">
-						<ClayDropDownWithItems
-							items={[
-								{
-									label: Liferay.Language.get('reorder-sets'),
-									onClick: () => {
-										setShowReorderModal(true);
-									},
-									symbolLeft: 'order-arrow',
+					<ClayDropDownWithItems
+						className="flex-shrink-0"
+						items={[
+							{
+								label: Liferay.Language.get('reorder-sets'),
+								onClick: () => {
+									setShowReorderModal(true);
 								},
-								{
-									disabled: displayStyleButtonDisabled,
-									label: viewButtonLabel,
-									onClick: () => {
-										setDisplayStyle(
-											displayStyle ===
-												FRAGMENTS_DISPLAY_STYLES.LIST
-												? FRAGMENTS_DISPLAY_STYLES.CARDS
-												: FRAGMENTS_DISPLAY_STYLES.LIST
-										);
-									},
-									symbolLeft:
-										displayStyleButtonDisabled ||
+								symbolLeft: 'order-arrow',
+							},
+							{
+								disabled: displayStyleButtonDisabled,
+								label: viewButtonLabel,
+								onClick: () => {
+									setDisplayStyle(
 										displayStyle ===
 											FRAGMENTS_DISPLAY_STYLES.LIST
-											? 'cards2'
-											: 'list',
+											? FRAGMENTS_DISPLAY_STYLES.CARDS
+											: FRAGMENTS_DISPLAY_STYLES.LIST
+									);
 								},
-							]}
-							renderMenuOnClick
-							trigger={
-								<ClayButtonWithIcon
-									aria-label={Liferay.Language.get(
-										'components-options'
-									)}
-									className="components-options ml-2"
-									data-tooltip-align="top"
-									displayType="unstyled"
-									size="sm"
-									symbol="ellipsis-v"
-									title={Liferay.Language.get(
-										'components-options'
-									)}
-								/>
-							}
-						/>
-
-						{Liferay.FeatureFlags['LPD-34938'] &&
-						permissions.VIEW_MARKETPLACE ? (
-							<MarketplaceButton
-								body={Liferay.Language.get(
-									'we-are-excited-to-share-that-marketplace-is-now-part-of-page-builder'
+								symbolLeft:
+									displayStyleButtonDisabled ||
+									displayStyle ===
+										FRAGMENTS_DISPLAY_STYLES.LIST
+										? 'cards2'
+										: 'list',
+							},
+						]}
+						renderMenuOnClick
+						trigger={
+							<ClayButtonWithIcon
+								aria-label={Liferay.Language.get(
+									'components-options'
 								)}
-								className="ml-1"
-								fragmentPortletNamespace={
-									config.fragmentPortletNamespace
-								}
-								fragmentsImportURL={config.fragmentsImportURL}
-								heading={Liferay.Language.get(
-									'marketplace-is-now-in-page-builder'
+								className="components-options ml-2"
+								data-tooltip-align="top"
+								displayType="unstyled"
+								size="sm"
+								symbol="ellipsis-v"
+								title={Liferay.Language.get(
+									'components-options'
 								)}
-								permissions={{
-									installFreeApps:
-										permissions.INSTALL_FREE_BUNDLED_APPS_MARKETPLACE,
-									manageFragmentsEntries:
-										permissions.MANAGE_FRAGMENT_ENTRIES,
-									purchaseAndInstallPaidApps:
-										permissions.PURCHASE_AND_INSTALL_PAID_APPS_MARKETPLACE,
-								}}
-								portletNamespace={config.portletNamespace}
 							/>
-						) : null}
-					</div>
+						}
+					/>
+
+					{Liferay.FeatureFlags['LPD-34938'] &&
+					permissions.VIEW_MARKETPLACE ? (
+						<MarketplaceButton
+							body={Liferay.Language.get(
+								'we-are-excited-to-share-that-marketplace-is-now-part-of-page-builder'
+							)}
+							className="ml-1"
+							fragmentPortletNamespace={
+								config.fragmentPortletNamespace
+							}
+							fragmentsImportURL={config.fragmentsImportURL}
+							heading={Liferay.Language.get(
+								'marketplace-is-now-in-page-builder'
+							)}
+							permissions={{
+								installFreeApps:
+									permissions.INSTALL_FREE_BUNDLED_APPS_MARKETPLACE,
+								manageFragmentsEntries:
+									permissions.MANAGE_FRAGMENT_ENTRIES,
+								purchaseAndInstallPaidApps:
+									permissions.PURCHASE_AND_INSTALL_PAID_APPS_MARKETPLACE,
+							}}
+							portletNamespace={config.portletNamespace}
+						/>
+					) : null}
 				</div>
 
 				{searchValue ? (
