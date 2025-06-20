5b598898b1ff910aee33dc8893726d91d06a2a37
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:55:36 +0200

    Revert "LPD-53882 Add validation rules to the space name field"
    This reverts commit b426a4e3f4283cccd9a750de5dabae1adef5068b.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/NewSpace.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/NewSpace.tsx
@@ -14,7 +14,7 @@ import SpaceService from '../../services/SpaceService';
 import SpaceColorDropdown from '../components/SpaceLogoColorDropdown';
 import SpaceSticker, {LogoColor} from '../components/SpaceSticker';
 import {FieldText} from '../components/forms';
-import {invalidCharacter, maxLength, notNull, nonNumeric, required, validate} from '../components/forms/validations';
+import {required, validate} from '../components/forms/validations';
 import {getImage} from '../util/getImage';
 import {NewSpaceFormSection} from './NewSpaceFormSection';
 
@@ -53,7 +53,7 @@ const NewSpace = ({baseRedirectUrl}: NewSpaceProps) => {
 		validate: (values) =>
 			validate(
 				{
-					name: [required, nonNumeric, notNull, invalidCharacter('*'), maxLength(150)],
+					name: [required],
 				},
 				values
 			),
