8ca8143996029e535a79e74fcc8c317c37530510
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 15:20:03 +0200

    Revert "LPD-49345 Add tests"
    This reverts commit 5bed2905f845ef75d97c2d856d8d9108a71e99a1.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/SpaceMembersInputWithSelect.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/SpaceMembersInputWithSelect.tsx
@@ -8,14 +8,14 @@ import '../../../css/spaces/SpaceMembersInputWithSelect.scss';
 import Autocomplete from '@clayui/autocomplete';
 import ClayForm, {ClayInput, ClaySelectWithOption} from '@clayui/form';
 import classNames from 'classnames';
-import React, {useId} from 'react';
+import React from 'react';
 
 export enum SelectOptions {
 	USERS = 'users',
 	GROUPS = 'groups',
 }
 
-export interface SpaceMembersInputWithSelectProps {
+interface SpaceMembersInputWithSelectProps {
 	className?: string;
 	inputValue?: string;
 	onInputChange?: (value: string) => void;
@@ -30,20 +30,17 @@ export function SpaceMembersInputWithSelect({
 	onSelectChange,
 	selectValue,
 }: SpaceMembersInputWithSelectProps) {
-	const selectId = useId();
-
 	return (
 		<ClayForm.Group
 			className={classNames('space-members-input-with-select', className)}
 		>
-			<label className="d-block" htmlFor={selectId}>
+			<label className="d-block" htmlFor="autocomplete">
 				{Liferay.Language.get('add-people-to-collaborate')}
 			</label>
 
 			<ClayInput.Group>
 				<ClayInput.GroupItem prepend shrink>
 					<ClaySelectWithOption
-						id={selectId}
 						onChange={(event) => {
 							onSelectChange?.(
 								event.target.value as SelectOptions
