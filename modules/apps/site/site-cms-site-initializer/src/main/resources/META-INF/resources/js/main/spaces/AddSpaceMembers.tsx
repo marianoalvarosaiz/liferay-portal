73616b0427be6e847705279405227ba01bf6e36d
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:55:05 +0200

    Revert "LPD-56418 Source formatting"
    This reverts commit 1a89cc82f9ca620f88a791619a520f7c677ec097.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/AddSpaceMembers.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/AddSpaceMembers.tsx
@@ -9,7 +9,7 @@ import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
 import ClayIcon from '@clayui/icon';
 import ClayLayout from '@clayui/layout';
 import ClaySticker from '@clayui/sticker';
-import {openToast} from 'frontend-js-components-web';
+import {openModal, openToast} from 'frontend-js-components-web';
 import {navigate, sub} from 'frontend-js-web';
 import React, {useEffect, useId, useState} from 'react';
 
@@ -94,7 +94,9 @@ export function AddSpaceMembers({
 						Liferay.Language.get(
 							'user-x-successfully-added-to-space-x'
 						),
-						[`<strong>${item.name}</strong>`]
+						[
+							`<strong>${item.name}</strong>`
+						]
 					),
 					type: 'success',
 				});
@@ -103,7 +105,9 @@ export function AddSpaceMembers({
 				openToast({
 					message: sub(
 						Liferay.Language.get('failed-to-add-user-x-to-space-x'),
-						[`<strong>${item.name}</strong>`]
+						[
+							`<strong>${item.name}</strong>`
+						]
 					),
 					type: 'danger',
 				});
@@ -126,10 +130,10 @@ export function AddSpaceMembers({
 
 			openToast({
 				message: sub(
-					Liferay.Language.get(
-						'group-x-successfully-added-to-space-x'
-					),
-					[`<strong>${item.name}</strong>`]
+					Liferay.Language.get('group-x-successfully-added-to-space-x'),
+					[
+						`<strong>${item.name}</strong>`
+					]
 				),
 				type: 'success',
 			});
@@ -138,7 +142,9 @@ export function AddSpaceMembers({
 			openToast({
 				message: sub(
 					Liferay.Language.get('failed-to-add-group-x-to-space-x'),
-					[`<strong>${item.name}</strong>`]
+					[
+						`<strong>${item.name}</strong>`
+					]
 				),
 				type: 'danger',
 			});
@@ -156,21 +162,20 @@ export function AddSpaceMembers({
 
 			openToast({
 				message: sub(
-					Liferay.Language.get(
-						'user-x-successfully-removed-from-space-x'
-					),
-					[`<strong>${user.name}</strong>`]
+					Liferay.Language.get('user-x-successfully-removed-from-space-x'),
+					[
+						`<strong>${user.name}</strong>`
+					]
 				),
 				type: 'success',
 			});
-		}
-		catch (error) {
+		} catch(error) {
 			openToast({
 				message: sub(
-					Liferay.Language.get(
-						'unable-to-remove-user-x-from-space-x'
-					),
-					[`<strong>${user.name}</strong>`]
+					Liferay.Language.get('unable-to-remove-user-x-from-space-x'),
+					[
+						`<strong>${user.name}</strong>`
+					]
 				),
 				type: 'success',
 			});
@@ -190,21 +195,20 @@ export function AddSpaceMembers({
 
 			openToast({
 				message: sub(
-					Liferay.Language.get(
-						'group-x-successfully-removed-from-space-x'
-					),
-					[`<strong>${group.name}</strong>`]
+					Liferay.Language.get('group-x-successfully-removed-from-space-x'),
+					[
+						`<strong>${group.name}</strong>`
+					]
 				),
 				type: 'success',
 			});
-		}
-		catch (error) {
+		} catch(error) {
 			openToast({
 				message: sub(
-					Liferay.Language.get(
-						'unable-to-remove-group-x-from-space-x'
-					),
-					[`<strong>${group.name}</strong>`]
+					Liferay.Language.get('unable-to-remove-group-x-from-space-x'),
+					[
+						`<strong>${group.name}</strong>`
+					]
 				),
 				type: 'success',
 			});
