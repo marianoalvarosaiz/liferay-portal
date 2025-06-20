430d3eb629b6d4f64447a3bcfad2ed31dde86f32
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:58:26 +0200

    Revert "LPD-55716 Simplify test definition"
    This reverts commit 0c2b894ea6d16e836843fa645969f7f7ab4e8933.

--- modules/test/playwright/tests/export-import-web/main/site.import.spec.ts
+++ modules/test/playwright/tests/export-import-web/main/site.import.spec.ts
@@ -100,6 +100,26 @@ const testWithDeprecationFF = mergeTests(
 	uiElementsPageTest
 );
 
+const testDataDeletionHiddenDeprecationFF = mergeTests(
+	dataApiHelpersTest,
+	exportImportPagesTest,
+	featureFlagsTest({
+		'LPD-44771': {enabled: false},
+	}),
+	isolatedSiteTest,
+	loginTest()
+);
+
+const testCopyAsNewHiddenDeprecationFF = mergeTests(
+	dataApiHelpersTest,
+	exportImportPagesTest,
+	featureFlagsTest({
+		'LPD-44307': {enabled: false},
+	}),
+	isolatedSiteTest,
+	loginTest()
+);
+
 async function getSiteHomePageScreenshot(
 	page: Page,
 	siteKey: string,
@@ -534,7 +554,7 @@ test('can see corresponding elements at site level', async ({
 	await expect(exportImportPage.page.getByText('Copy as New:')).toBeVisible();
 });
 
-testWithDeprecationFFDisabled(
+testDataDeletionHiddenDeprecationFF(
 	"hides 'Delete Application Data' checkbox when deprecation FF is false",
 	{tag: ['@LPD-44771']},
 	async ({apiHelpers, exportImportPage}) => {
@@ -575,7 +595,7 @@ testWithDeprecationFFDisabled(
 	}
 );
 
-testWithDeprecationFFDisabled(
+testCopyAsNewHiddenDeprecationFF(
 	"hides 'Copy as new' radio button when deprecation FF is disabled",
 	{tag: ['@LPD-44307']},
 	async ({apiHelpers, exportImportPage}) => {
