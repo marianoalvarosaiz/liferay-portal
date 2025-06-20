6a0b10dd64442c0e8a4f230e5d771d3da6e25fe6
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:59:00 +0200

    Revert "LPD-57500 Add groupId to incomplete entry"
    This reverts commit 23bf6353a9bea1338576914ee1e201752f79d469.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -470,12 +470,12 @@ public class ObjectEntryLocalServiceImpl
 
 	@Override
 	public ObjectEntry addObjectEntry(
-			String externalReferenceCode, long groupId, long userId,
+			String externalReferenceCode, long userId,
 			ObjectDefinition objectDefinition, long objectEntryFolderId)
 		throws PortalException {
 
 		return _addObjectEntry(
-			externalReferenceCode, groupId, userId,
+			externalReferenceCode, userId,
 			objectDefinition.getObjectDefinitionId(), objectEntryFolderId,
 			WorkflowConstants.STATUS_DRAFT);
 	}
@@ -1250,8 +1250,7 @@ public class ObjectEntryLocalServiceImpl
 	@Override
 	@Transactional(propagation = Propagation.REQUIRED)
 	public ObjectEntry getOrAddIncompleteObjectEntry(
-			String externalReferenceCode, long groupId, long userId,
-			long objectDefinitionId)
+			String externalReferenceCode, long userId, long objectDefinitionId)
 		throws PortalException {
 
 		ObjectEntry objectEntry = fetchObjectEntry(
@@ -1274,7 +1273,7 @@ public class ObjectEntryLocalServiceImpl
 		}
 
 		objectEntry = _addObjectEntry(
-			externalReferenceCode, groupId, userId, objectDefinitionId,
+			externalReferenceCode, userId, objectDefinitionId,
 			ObjectEntryFolderConstants.PARENT_OBJECT_ENTRY_FOLDER_ID_DEFAULT,
 			WorkflowConstants.STATUS_INCOMPLETE);
 
@@ -2368,16 +2367,14 @@ public class ObjectEntryLocalServiceImpl
 	}
 
 	private ObjectEntry _addObjectEntry(
-			String externalReferenceCode, long groupId, long userId,
-			long objectDefinitionId, long objectEntryFolderId, int status)
+			String externalReferenceCode, long userId, long objectDefinitionId,
+			long objectEntryFolderId, int status)
 		throws PortalException {
 
 		ObjectEntry objectEntry = objectEntryPersistence.create(
 			counterLocalService.increment());
 
 		objectEntry.setExternalReferenceCode(externalReferenceCode);
-
-		objectEntry.setGroupId(groupId);
 
 		User user = _userLocalService.getUser(userId);
 
