e6af3f27f2590a69c68dadf5f25f8180e7e155d4
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:55:29 +0200

    Revert "LPD-49345 Create AddSpaceMember page"
    This reverts commit e4c69e6b48a74d070347987d00b9285a763cfd34.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/AddSpaceMembers.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/AddSpaceMembers.tsx
@@ -3,68 +3,14 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import ClayButton from '@clayui/button';
-import ClayLayout from '@clayui/layout';
-import {sub} from 'frontend-js-web';
-import React, {useState} from 'react';
-
-import {getImage} from '../util/getImage';
-import {NewSpaceFormSection} from './NewSpaceFormSection';
-import {
-	SelectOptions,
-	SpaceMembersInputWithSelect,
-} from './SpaceMembersInputWithSelect';
+import React from 'react';
 
 export interface AddSpaceMembersProps {
 	assetLibraryId: string;
-	spaceName: string;
 }
 
 const AddSpaceMembers = ({assetLibraryId}: AddSpaceMembersProps) => {
-	const [selectedOption, setSelectedOption] = useState(SelectOptions.USERS);
-	const [inputValue, setInputValue] = useState('');
-
-	return (
-		<ClayLayout.Row className="p-4">
-			<ClayLayout.Col className="mw-50 px-9 w-50">
-				<NewSpaceFormSection
-					description={Liferay.Language.get(
-						'add-team-members-to-this-space-to-start-collaborating'
-					)}
-					linkLabel={Liferay.Language.get(
-						'learn-more-about-memberships'
-					)}
-					linkUrl="/"
-					onSubmit={() => null}
-					step={2}
-					title={sub(
-						Liferay.Language.get('add-members-to-x'),
-						assetLibraryId
-					)}
-				>
-					<SpaceMembersInputWithSelect
-						inputValue={inputValue}
-						onInputChange={setInputValue}
-						onSelectChange={setSelectedOption}
-						selectValue={selectedOption}
-					/>
-
-					<ClayButton.Group className="mb-0 w-100" spaced vertical>
-						<ClayButton className="mt-4">
-							{Liferay.Language.get('continue-without-members')}
-						</ClayButton>
-					</ClayButton.Group>
-				</NewSpaceFormSection>
-			</ClayLayout.Col>
-
-			<ClayLayout.Col>
-				<img
-					aria-hidden="true"
-					src={getImage('add_space_members_illustration.svg')}
-				></img>
-			</ClayLayout.Col>
-		</ClayLayout.Row>
-	);
+	return <h1>Hello World - {assetLibraryId}</h1>;
 };
 
 export default AddSpaceMembers;
