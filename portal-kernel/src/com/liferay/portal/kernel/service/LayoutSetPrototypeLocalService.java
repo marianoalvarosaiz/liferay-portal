79bab9687188e0f0a5ec38acf234471926991fa7
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 15:09:59 +0200

    Revert "LPD-57290 Regen"
    This reverts commit 396c6122b959505f50240ae90fdfb78e475bc515.

--- portal-kernel/src/com/liferay/portal/kernel/service/LayoutSetPrototypeLocalService.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/LayoutSetPrototypeLocalService.java
@@ -6,9 +6,7 @@
 package com.liferay.portal.kernel.service;
 
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
-import com.liferay.petra.function.UnsafeFunction;
 import com.liferay.petra.sql.dsl.query.DSLQuery;
-import com.liferay.portal.kernel.change.tracking.CTAware;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
@@ -21,8 +19,6 @@ import com.liferay.portal.kernel.model.PersistedModel;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
-import com.liferay.portal.kernel.service.change.tracking.CTService;
-import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
 import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Isolation;
@@ -48,7 +44,6 @@ import org.osgi.annotation.versioning.ProviderType;
  * @see LayoutSetPrototypeLocalServiceUtil
  * @generated
  */
-@CTAware
 @OSGiBeanProperties(
 	property = {
 		"model.class.name=com.liferay.portal.kernel.model.LayoutSetPrototype"
@@ -60,8 +55,7 @@ import org.osgi.annotation.versioning.ProviderType;
 	rollbackFor = {PortalException.class, SystemException.class}
 )
 public interface LayoutSetPrototypeLocalService
-	extends BaseLocalService, CTService<LayoutSetPrototype>,
-			PersistedModelLocalService {
+	extends BaseLocalService, PersistedModelLocalService {
 
 	/*
 	 * NOTE FOR DEVELOPERS:
@@ -359,20 +353,5 @@ public interface LayoutSetPrototypeLocalService
 	public LayoutSetPrototype updateLayoutSetPrototype(
 			long layoutSetPrototypeId, String settings)
 		throws PortalException;
-
-	@Override
-	@Transactional(enabled = false)
-	public CTPersistence<LayoutSetPrototype> getCTPersistence();
-
-	@Override
-	@Transactional(enabled = false)
-	public Class<LayoutSetPrototype> getModelClass();
-
-	@Override
-	@Transactional(rollbackFor = Throwable.class)
-	public <R, E extends Throwable> R updateWithUnsafeFunction(
-			UnsafeFunction<CTPersistence<LayoutSetPrototype>, R, E>
-				updateUnsafeFunction)
-		throws E;
 
 }
