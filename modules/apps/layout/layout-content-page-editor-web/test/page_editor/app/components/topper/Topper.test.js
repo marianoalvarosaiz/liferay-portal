bfe8b0b17400b29e570746878ad20654942b6ec7
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 15:14:39 +0200

    Revert "LPD-58113 Unify sub function for all tests"
    This reverts commit 1bcf77d8daca198b944222ddf39bb05a2fcc1279.

--- modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/topper/Topper.test.js
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/components/topper/Topper.test.js
@@ -37,6 +37,11 @@ jest.mock(
 	}
 );
 
+jest.mock('frontend-js-web', () => ({
+	...jest.requireActual('frontend-js-web'),
+	sub: jest.fn((langKey, arg) => langKey.replace('x', arg)),
+}));
+
 const LAYOUT_DATA = {
 	items: {
 		itemId: {
