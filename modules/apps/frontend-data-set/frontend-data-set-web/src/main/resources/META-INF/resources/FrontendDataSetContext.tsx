a6161eb97d92d838420419de8a0f7cdcc5a415ae
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:57:09 +0200

    Revert "LPD-57643 Migrate main exports to TS, move TS types to standard location"
    This reverts commit 750c24b8c148c7ff9521f958db1aec1cf6effaba.

--- modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/FrontendDataSetContext.tsx
+++ modules/apps/frontend-data-set/frontend-data-set-web/src/main/resources/META-INF/resources/FrontendDataSetContext.tsx
@@ -5,7 +5,7 @@
 
 import React from 'react';
 
-import {IInlineEditingSettings, IItemsActions, ISchema} from './utils/types';
+import {IInlineEditingSettings, IItemsActions, ISchema} from '.';
 
 export interface IFrontendDataSetContext {
 	actionParameterName?: string | null;
