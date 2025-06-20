9b6047172bd571859cffccd623d5baf038e3371a
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-20 14:55:58 +0200

    Revert "LPD-57456 Update current tests"
    This reverts commit d25da3f021a7d14344e0850d5482e33768847a6a.

--- modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/test/js/DocumentLibraryOpener.es.js
+++ modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/test/js/DocumentLibraryOpener.es.js
@@ -3,29 +3,20 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {openModal} from 'frontend-js-components-web';
+import {openWindow} from 'frontend-js-web';
 
 import {DocumentLibraryOpener} from '../../src/main/resources/META-INF/resources/js/index';
 
 const realSetTimeout = setTimeout;
 
 jest.mock('frontend-js-components-web', () => ({
-	openModal: jest.fn((options) => {
-		setTimeout(() => {
-			if (options.onOpen) {
-				options.onOpen();
-			}
-		}, 0);
-
-		return {
-			unmount: jest.fn(),
-		};
-	}),
 	openToast: jest.fn(),
 }));
 
 jest.mock('frontend-js-web', () => ({
 	...jest.requireActual('frontend-js-web'),
+	getWindow: jest.fn(() => ({hide: jest.fn()})),
+	openWindow: jest.fn().mockImplementation((_, callback) => callback()),
 }));
 
 function replyAndWait({body = {}, ms}) {
@@ -92,8 +83,10 @@ describe('DocumentLibraryOpener', () => {
 			});
 
 			it('opens the loading modal', () => {
-				expect(openModal).toHaveBeenCalledTimes(1);
-				expect(openModal.mock.calls[0][0].bodyHTML).toContain(
+				expect(openWindow).toHaveBeenCalledTimes(1);
+				expect(
+					openWindow.mock.calls[0][0].dialog.bodyContent
+				).toContain(
 					'you-are-being-redirected-to-an-external-editor-to-edit-this-document'
 				);
 			});
@@ -143,8 +136,10 @@ describe('DocumentLibraryOpener', () => {
 			});
 
 			it('opens the loading modal', () => {
-				expect(openModal).toHaveBeenCalledTimes(1);
-				expect(openModal.mock.calls[0][0].bodyHTML).toContain(
+				expect(openWindow).toHaveBeenCalledTimes(1);
+				expect(
+					openWindow.mock.calls[0][0].dialog.bodyContent
+				).toContain(
 					'you-are-being-redirected-to-an-external-editor-to-edit-this-document'
 				);
 			});
@@ -197,8 +192,10 @@ describe('DocumentLibraryOpener', () => {
 			});
 
 			it('opens the loading modal', () => {
-				expect(openModal).toHaveBeenCalledTimes(1);
-				expect(openModal.mock.calls[0][0].bodyHTML).toContain(
+				expect(openWindow).toHaveBeenCalledTimes(1);
+				expect(
+					openWindow.mock.calls[0][0].dialog.bodyContent
+				).toContain(
 					'you-are-being-redirected-to-an-external-editor-to-edit-this-document'
 				);
 			});
@@ -242,8 +239,10 @@ describe('DocumentLibraryOpener', () => {
 			});
 
 			it('opens the loading modal with the creation message', () => {
-				expect(openModal).toHaveBeenCalledTimes(1);
-				expect(openModal.mock.calls[0][0].bodyHTML).toContain(
+				expect(openWindow).toHaveBeenCalledTimes(1);
+				expect(
+					openWindow.mock.calls[0][0].dialog.bodyContent
+				).toContain(
 					'you-are-being-redirected-to-an-external-editor-to-create-this-document'
 				);
 			});
@@ -283,8 +282,10 @@ describe('DocumentLibraryOpener', () => {
 			});
 
 			it('opens the loading modal with the creation message', () => {
-				expect(openModal).toHaveBeenCalledTimes(1);
-				expect(openModal.mock.calls[0][0].bodyHTML).toContain(
+				expect(openWindow).toHaveBeenCalledTimes(1);
+				expect(
+					openWindow.mock.calls[0][0].dialog.bodyContent
+				).toContain(
 					'you-are-being-redirected-to-an-external-editor-to-create-this-document'
 				);
 			});
