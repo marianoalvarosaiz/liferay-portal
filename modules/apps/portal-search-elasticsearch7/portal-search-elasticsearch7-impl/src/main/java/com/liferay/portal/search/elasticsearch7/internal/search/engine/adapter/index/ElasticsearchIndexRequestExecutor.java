b7e43d367e93de7bae048ae6a4f0a433f8e6ad84
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-21 10:29:32 +0200

    Revert "LPD-58417 Merge GetFieldMappingIndexRequestExecutor/GetFieldMappingIndexRequestExecutorImpl into pojos."
    This reverts commit 8da34010f38478f92da664c5772459a60f30e8bb.

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/index/ElasticsearchIndexRequestExecutor.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/index/ElasticsearchIndexRequestExecutor.java
@@ -161,9 +161,6 @@ public class ElasticsearchIndexRequestExecutor implements IndexRequestExecutor {
 			_elasticsearchClientResolver);
 		_flushIndexRequestExecutor = new FlushIndexRequestExecutor(
 			_elasticsearchClientResolver);
-		_getFieldMappingIndexRequestExecutor =
-			new GetFieldMappingIndexRequestExecutor(
-				_elasticsearchClientResolver, _jsonFactory);
 		_getMappingIndexRequestExecutor = new GetMappingIndexRequestExecutor(
 			_elasticsearchClientResolver);
 		_indicesExistsIndexRequestExecutor =
@@ -189,6 +186,8 @@ public class ElasticsearchIndexRequestExecutor implements IndexRequestExecutor {
 	private ElasticsearchClientResolver _elasticsearchClientResolver;
 
 	private FlushIndexRequestExecutor _flushIndexRequestExecutor;
+
+	@Reference
 	private GetFieldMappingIndexRequestExecutor
 		_getFieldMappingIndexRequestExecutor;
 
