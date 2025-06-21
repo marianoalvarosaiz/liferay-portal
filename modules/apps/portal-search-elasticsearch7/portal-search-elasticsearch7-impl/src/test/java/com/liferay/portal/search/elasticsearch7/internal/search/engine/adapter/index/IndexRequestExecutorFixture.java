4cbeb4e7e2b10746b86ce8af1cb94348a94c836c
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-21 10:29:32 +0200

    Revert "LPD-58417 Fix tests"
    This reverts commit 344ffaab1713b6a2e0924b3c9f43d6e56abeac33.

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/index/IndexRequestExecutorFixture.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/index/IndexRequestExecutorFixture.java
@@ -28,7 +28,7 @@ public class IndexRequestExecutorFixture {
 
 		ReflectionTestUtil.setFieldValue(
 			_indexRequestExecutor, "_closeIndexRequestExecutor",
-			new CloseIndexRequestExecutor(_elasticsearchClientResolver));
+			_createCloseIndexRequestExecutor(_elasticsearchClientResolver));
 
 		ReflectionTestUtil.setFieldValue(
 			_indexRequestExecutor, "_createIndexRequestExecutor",
@@ -73,6 +73,19 @@ public class IndexRequestExecutorFixture {
 		ElasticsearchClientResolver elasticsearchClientResolver) {
 
 		_elasticsearchClientResolver = elasticsearchClientResolver;
+	}
+
+	private CloseIndexRequestExecutor _createCloseIndexRequestExecutor(
+		ElasticsearchClientResolver elasticsearchClientResolver) {
+
+		CloseIndexRequestExecutor closeIndexRequestExecutor =
+			new CloseIndexRequestExecutorImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			closeIndexRequestExecutor, "_elasticsearchClientResolver",
+			elasticsearchClientResolver);
+
+		return closeIndexRequestExecutor;
 	}
 
 	private CreateIndexRequestExecutor _createCreateIndexRequestExecutor(
