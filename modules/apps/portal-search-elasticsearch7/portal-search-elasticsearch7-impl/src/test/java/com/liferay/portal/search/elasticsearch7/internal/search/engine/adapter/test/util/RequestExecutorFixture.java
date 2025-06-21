9621fc77b6df1a29ab05d91aabaeddf14b84768e
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-21 10:29:36 +0200

    Revert "LPD-58417 Fix tests"
    This reverts commit 2deaae23926a8ec573310ba94f8256be10e0b8c5.

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/test/util/RequestExecutorFixture.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/test/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/test/util/RequestExecutorFixture.java
@@ -21,6 +21,7 @@ import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.d
 import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.index.CreateIndexRequestExecutor;
 import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.index.CreateIndexRequestExecutorImpl;
 import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.index.DeleteIndexRequestExecutor;
+import com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.index.DeleteIndexRequestExecutorImpl;
 import com.liferay.portal.search.engine.adapter.document.GetDocumentRequest;
 import com.liferay.portal.search.engine.adapter.document.GetDocumentResponse;
 import com.liferay.portal.search.engine.adapter.document.IndexDocumentRequest;
@@ -106,7 +107,10 @@ public class RequestExecutorFixture {
 			_createIndexRequestExecutor, "_elasticsearchClientResolver",
 			_elasticsearchClientResolver);
 
-		_deleteIndexRequestExecutor = new DeleteIndexRequestExecutor(
+		_deleteIndexRequestExecutor = new DeleteIndexRequestExecutorImpl();
+
+		ReflectionTestUtil.setFieldValue(
+			_deleteIndexRequestExecutor, "_elasticsearchClientResolver",
 			_elasticsearchClientResolver);
 
 		_getDocumentRequestExecutor = new GetDocumentRequestExecutorImpl();
