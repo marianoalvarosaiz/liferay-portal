44c12a68434a498194b3394469ce48bf4f9b6052
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-21 10:28:31 +0200

    Revert "LPD-56418 Integrate autocomplete with useResource response to display user/groups"
    This reverts commit 8dfd85eb26a17c8a0dea19195ec9c65f0cb8f0b4.

--- modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/AddSpaceMembers.tsx
+++ modules/apps/site/site-cms-site-initializer/src/main/resources/META-INF/resources/js/main/spaces/AddSpaceMembers.tsx
@@ -3,13 +3,10 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import '../../../css/spaces/AddSpaceMembers.scss';
-
 import ClayButton from '@clayui/button';
 import ClayLayout from '@clayui/layout';
 import {sub} from 'frontend-js-web';
 import React, {useState} from 'react';
-import ClaySticker from '@clayui/sticker';
 
 import {getImage} from '../util/getImage';
 import {NewSpaceFormSection} from './NewSpaceFormSection';
@@ -17,7 +14,6 @@ import {
 	SelectOptions,
 	SpaceMembersInputWithSelect,
 } from './SpaceMembersInputWithSelect';
-import { UserAccount, UserGroup } from '../../types/UserAccount';
 
 export interface AddSpaceMembersProps {
 	spaceName: string;
@@ -26,37 +22,9 @@ export interface AddSpaceMembersProps {
 export function AddSpaceMembers({spaceName}: AddSpaceMembersProps) {
 	const [selectedOption, setSelectedOption] = useState(SelectOptions.USERS);
 	const [inputValue, setInputValue] = useState('');
-	const [selectedUsers, setSelectedUsers] = useState<UserAccount[]>([]);
-	const [selectedUserGroups, setSelectedUserGroups] = useState<UserGroup[]>([]);
-
-	const onAutocompleteItemSelected = (item: UserAccount | UserGroup) => {
-		if (selectedOption === SelectOptions.USERS) {
-			if(selectedUsers.some((user) => user.id === item.id)) {
-				return;
-			}
-
-			setSelectedUsers([...selectedUsers, item as UserAccount]);
-		} else {
-			if(selectedUserGroups.some((group) => group.id === item.id)){
-				return;
-			}
-
-			setSelectedUserGroups([...selectedUserGroups, item]);
-		}
-	}
-
-	const onContinueBtnClick = () => {
-		if(selectedUsers.length) {
-			// Call endpoint for users
-		}
-
-		if(selectedUserGroups.length) {
-			// Call endpoint for groups
-		}
-	}
 
 	return (
-		<ClayLayout.Row className="add-space-members">
+		<ClayLayout.Row className="p-4">
 			<ClayLayout.Col className="mw-50 px-9 w-50">
 				<NewSpaceFormSection
 					description={Liferay.Language.get(
@@ -78,48 +46,10 @@ export function AddSpaceMembers({spaceName}: AddSpaceMembersProps) {
 						onInputChange={setInputValue}
 						onSelectChange={setSelectedOption}
 						selectValue={selectedOption}
-						onAutocompleteItemSelected={onAutocompleteItemSelected}
 					/>
 
-					<label className="d-block" htmlFor="list-of-users">Who has access</label>
-					<ul className="members-list" id="list-of-users">
-						{selectedUsers.map((user) => (
-							<li>
-								<ClaySticker
-									displayType="primary"
-									shape="circle"
-									size="sm"
-								>
-									<img
-										alt={user.name}
-										className="sticker-img"
-										src={user.image || '/image/user_portrait'}
-									/>
-								</ClaySticker>
-								<span className="ml-2">
-									{user.name} ({user.emailAddress?.split('@')[0]})
-								</span>
-							</li>
-						))}
-						{selectedUserGroups.map((group) => (
-							<li>
-								<ClaySticker
-									displayType="primary"
-									shape="circle"
-									size="sm"
-								>
-									<img
-										alt={group.name}
-										className="sticker-img"
-										src={'/image/user_portrait'}
-									/>
-								</ClaySticker>
-								<span className="ml-2">{group.name}</span>
-							</li>
-						))}
-					</ul>
 					<ClayButton.Group className="mb-0 w-100" spaced vertical>
-						<ClayButton className="mt-4" onClick={onContinueBtnClick}>
+						<ClayButton className="mt-4">
 							{Liferay.Language.get('continue-without-members')}
 						</ClayButton>
 					</ClayButton.Group>
