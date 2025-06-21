1d81c6b9ee9987664cb74a9b2abf6a44974e1491
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-21 10:36:24 +0200

    Revert "LPD-58424 Merge CreateSnapshotRequestExecutor/CreateSnapshotRequestExecutorImpl into pojos"
    This reverts commit 81c6cdd53f99535440baba162a620520415d0cc9.

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/snapshot/ElasticsearchSnapshotRequestExecutor.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/search/engine/adapter/snapshot/ElasticsearchSnapshotRequestExecutor.java
@@ -5,7 +5,6 @@
 
 package com.liferay.portal.search.elasticsearch7.internal.search.engine.adapter.snapshot;
 
-import com.liferay.portal.search.elasticsearch7.internal.connection.ElasticsearchClientResolver;
 import com.liferay.portal.search.engine.adapter.snapshot.CreateSnapshotRepositoryRequest;
 import com.liferay.portal.search.engine.adapter.snapshot.CreateSnapshotRepositoryResponse;
 import com.liferay.portal.search.engine.adapter.snapshot.CreateSnapshotRequest;
@@ -20,7 +19,6 @@ import com.liferay.portal.search.engine.adapter.snapshot.RestoreSnapshotRequest;
 import com.liferay.portal.search.engine.adapter.snapshot.RestoreSnapshotResponse;
 import com.liferay.portal.search.engine.adapter.snapshot.SnapshotRequestExecutor;
 
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -78,16 +76,11 @@ public class ElasticsearchSnapshotRequestExecutor
 		return restoreSnapshotRequestExecutor.execute(restoreSnapshotRequest);
 	}
 
-	@Activate
-	protected void activate() {
-		createSnapshotRequestExecutor = new CreateSnapshotRequestExecutor(
-			_elasticsearchClientResolver);
-	}
-
 	@Reference
 	protected CreateSnapshotRepositoryRequestExecutor
 		createSnapshotRepositoryRequestExecutor;
 
+	@Reference
 	protected CreateSnapshotRequestExecutor createSnapshotRequestExecutor;
 
 	@Reference
@@ -102,8 +95,5 @@ public class ElasticsearchSnapshotRequestExecutor
 
 	@Reference
 	protected RestoreSnapshotRequestExecutor restoreSnapshotRequestExecutor;
-
-	@Reference
-	private ElasticsearchClientResolver _elasticsearchClientResolver;
 
 }
