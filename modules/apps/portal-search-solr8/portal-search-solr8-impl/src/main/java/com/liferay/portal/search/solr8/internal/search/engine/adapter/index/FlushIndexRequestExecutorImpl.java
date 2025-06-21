da37d4e7ad8308e5a9b4c54742c99ee636bbc4c8
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-06-21 10:29:34 +0200

    Revert "LPD-58417 Merge FlushIndexRequestExecutor/FlushIndexRequestExecutorImpl into pojos"
    This reverts commit e900be2960f9a771d9191acfab3d13a45749769d.

--- modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/search/engine/adapter/index/FlushIndexRequestExecutorImpl.java
+++ modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/search/engine/adapter/index/FlushIndexRequestExecutorImpl.java
@@ -8,11 +8,18 @@ package com.liferay.portal.search.solr8.internal.search.engine.adapter.index;
 import com.liferay.portal.search.engine.adapter.index.FlushIndexRequest;
 import com.liferay.portal.search.engine.adapter.index.FlushIndexResponse;
 
+import org.osgi.service.component.annotations.Component;
+
 /**
  * @author Bryan Engler
  */
-public interface FlushIndexRequestExecutor {
+@Component(service = FlushIndexRequestExecutor.class)
+public class FlushIndexRequestExecutorImpl
+	implements FlushIndexRequestExecutor {
 
-	public FlushIndexResponse execute(FlushIndexRequest flushIndexRequest);
+	@Override
+	public FlushIndexResponse execute(FlushIndexRequest flushIndexRequest) {
+		throw new UnsupportedOperationException();
+	}
 
 }
