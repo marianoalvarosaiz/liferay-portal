0cf58eff76baf3212995701b633b2dc79d06f09c
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-07-02 12:34:07 +0200

    Revert "LPD-59251 Remove unnecessary LogWrapper wrapping"
    This reverts commit bb30489cecc3a118b1cde865248c3181059ad901.

--- portal-kernel/src/com/liferay/portal/kernel/log/LogFactoryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/log/LogFactoryUtil.java
@@ -22,23 +22,36 @@ public class LogFactoryUtil {
 	}
 
 	public static Log getLog(String name) {
-		Log log = _logs.get(name);
 
-		if (log == null) {
-			log = _logFactory.getLog(name);
+		// The following concurrent collection retrieve has the side effect of a
+		// memory fence read. This will invalidate all dirty cache data if there
+		// are any. If the LogWrapper swap happens before this, the new Log will
+		// be visible to the current Thread.
+
+		LogWrapper logWrapper = _logWrappers.get(name);
+
+		if (logWrapper == null) {
+			Log log = _logFactory.getLog(name);
 
 			if (SanitizerLogWrapper.isEnabled()) {
-				log = new SanitizerLogWrapper(log);
+				logWrapper = new SanitizerLogWrapper(log);
+			}
+			else if (log instanceof LogWrapper) {
+				logWrapper = (LogWrapper)log;
+			}
+			else {
+				logWrapper = new LogWrapper(_logFactory.getLog(name));
 			}
 
-			Log previousLog = _logs.putIfAbsent(name, log);
+			LogWrapper previousLogWrapper = _logWrappers.putIfAbsent(
+				name, logWrapper);
 
-			if (previousLog != null) {
-				log = previousLog;
+			if (previousLogWrapper != null) {
+				logWrapper = previousLogWrapper;
 			}
 		}
 
-		return log;
+		return logWrapper;
 	}
 
 	public static LogFactory getLogFactory() {
@@ -46,7 +59,7 @@ public class LogFactoryUtil {
 	}
 
 	private static final LogFactory _logFactory = new Log4jLogFactoryImpl();
-	private static final ConcurrentMap<String, Log> _logs =
+	private static final ConcurrentMap<String, LogWrapper> _logWrappers =
 		new ConcurrentHashMap<>();
 
 }
