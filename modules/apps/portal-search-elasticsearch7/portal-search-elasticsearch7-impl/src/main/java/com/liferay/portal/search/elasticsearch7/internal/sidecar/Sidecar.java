d6f53f802398f992196b46ec4263bb322341e7a3
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-07-02 12:33:54 +0200

    Revert "LPD-59251 Be quiet on normal shutdown"
    This reverts commit 6464908f209a22b2a3c8e1e48fb3f887950c98aa.

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/sidecar/Sidecar.java
@@ -116,8 +116,8 @@ public class Sidecar {
 	}
 
 	public void stop() {
-		if (_log.isDebugEnabled()) {
-			_log.debug("Stopping sidecar Elasticsearch");
+		if (_log.isInfoEnabled()) {
+			_log.info("Stopping sidecar Elasticsearch");
 		}
 
 		if (_processChannel != null) {
