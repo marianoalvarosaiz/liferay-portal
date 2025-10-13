7b2fe3952ae26a66d432f6d10b85a9e1578e778b
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-10-13 09:40:46 +0200

    LPD-66054 Use modified layout.

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/layout/prototype/DefaultSearchLayoutPrototypeCustomizer.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/layout/prototype/DefaultSearchLayoutPrototypeCustomizer.java
@@ -7,6 +7,7 @@ package com.liferay.portal.search.web.internal.layout.prototype;
 
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.util.DefaultLayoutPrototypesUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.search.web.constants.SearchBarPortletKeys;
@@ -33,67 +34,67 @@ public class DefaultSearchLayoutPrototypeCustomizer
 	public void customize(Layout layout) throws Exception {
 		String portletInstanceId = PortletIdCodec.generateInstanceId();
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				SearchBarPortletKeys.SEARCH_BAR, portletInstanceId),
 			"column-1");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				SuggestionsPortletKeys.SUGGESTIONS, portletInstanceId),
 			"column-1");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				SearchResultsPortletKeys.SEARCH_RESULTS, portletInstanceId),
 			"column-3");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				SearchOptionsPortletKeys.SEARCH_OPTIONS, portletInstanceId),
 			"column-3");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				SiteFacetPortletKeys.SITE_FACET, portletInstanceId),
 			"column-2");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				TypeFacetPortletKeys.TYPE_FACET, portletInstanceId),
 			"column-2");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				TagFacetPortletKeys.TAG_FACET, portletInstanceId),
 			"column-2");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				CategoryFacetPortletKeys.CATEGORY_FACET, portletInstanceId),
 			"column-2");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				FolderFacetPortletKeys.FOLDER_FACET, portletInstanceId),
 			"column-2");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				UserFacetPortletKeys.USER_FACET, portletInstanceId),
 			"column-2");
 
-		_addBorderlessPortlet(
+		layout = _addBorderlessPortlet(
 			layout,
 			PortletIdCodec.encode(
 				ModifiedFacetPortletKeys.MODIFIED_FACET, portletInstanceId),
@@ -105,7 +106,7 @@ public class DefaultSearchLayoutPrototypeCustomizer
 		return "1_2_columns_i";
 	}
 
-	private void _addBorderlessPortlet(
+	private Layout _addBorderlessPortlet(
 			Layout layout, String portletKey, String columnId)
 		throws Exception {
 
@@ -117,6 +118,9 @@ public class DefaultSearchLayoutPrototypeCustomizer
 			HashMapBuilder.put(
 				"portletSetupPortletDecoratorId", "borderless"
 			).build());
+
+		return LayoutLocalServiceUtil.updateTypeSettings(
+			layout, layout.getTypeSettings());
 	}
 
 }
