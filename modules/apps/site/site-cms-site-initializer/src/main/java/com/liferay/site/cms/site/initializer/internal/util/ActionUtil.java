2a577aba759aa3bdff72b7c7a6abc6c74480ad4d
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-10-14 11:04:27 +0200

    LPD-66054 Use new method that receives the layout. It is safe to pass directly the layout since we know it is the latest one.

--- modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/util/ActionUtil.java
+++ modules/apps/site/site-cms-site-initializer/src/main/java/com/liferay/site/cms/site/initializer/internal/util/ActionUtil.java
@@ -1096,10 +1096,8 @@ public class ActionUtil {
 		typeSettingsUnicodeProperties.put(
 			LayoutTypeSettingsConstants.KEY_PUBLISHED, Boolean.TRUE.toString());
 
-		LayoutLocalServiceUtil.updateLayout(
-			draftLayout.getGroupId(), draftLayout.isPrivateLayout(),
-			draftLayout.getLayoutId(),
-			typeSettingsUnicodeProperties.toString());
+		LayoutLocalServiceUtil.updateTypeSettings(
+			draftLayout, typeSettingsUnicodeProperties.toString());
 
 		return layoutPageTemplateEntry;
 	}
