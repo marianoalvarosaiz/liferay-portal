5d88f65f07ec58e0c1ae485d190cd45d53307dba
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2025-10-24 20:05:51 +0200

    Just a test

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFieldLocalServiceImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/service/impl/DDMFieldLocalServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 import com.liferay.dynamic.data.mapping.model.UnlocalizedValue;
 import com.liferay.dynamic.data.mapping.model.Value;
 import com.liferay.dynamic.data.mapping.model.impl.DDMFieldAttributeImpl;
+import com.liferay.dynamic.data.mapping.model.impl.DDMFieldImpl;
 import com.liferay.dynamic.data.mapping.service.base.DDMFieldLocalServiceBaseImpl;
 import com.liferay.dynamic.data.mapping.service.persistence.DDMFieldAttributePersistence;
 import com.liferay.dynamic.data.mapping.service.persistence.DDMStructurePersistence;
@@ -34,6 +35,8 @@ import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
 import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.dao.orm.EntityCache;
+import com.liferay.portal.kernel.dao.orm.Session;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONException;
 import com.liferay.portal.kernel.json.JSONFactory;
@@ -42,6 +45,7 @@ import com.liferay.portal.kernel.json.JSONSerializer;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.persistence.change.tracking.helper.CTPersistenceHelper;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -376,51 +380,82 @@ public class DDMFieldLocalServiceImpl extends DDMFieldLocalServiceBaseImpl {
 
 		ddmFormUpdateContext._ddmFieldEntries.addAll(childrenDDMFields);
 
-		for (Map.Entry<DDMField, DDMFieldInfo> entry :
-				ddmFormUpdateContext._ddmFieldEntries) {
+		List<DDMField> addedDDMFields = new ArrayList<>();
+		List<DDMField> obsoleteDDMFields = new ArrayList<>();
 
-			DDMField ddmField = entry.getKey();
-			DDMFieldInfo ddmFieldInfo = entry.getValue();
+		Session session = ddmFieldPersistence.openSession();
 
-			if (ddmFieldInfo == null) {
-				ddmFieldPersistence.remove(ddmField);
+		try {
+			for (Map.Entry<DDMField, DDMFieldInfo> entry :
+					ddmFormUpdateContext._ddmFieldEntries) {
 
-				continue;
+				DDMField ddmField = entry.getKey();
+				DDMFieldInfo ddmFieldInfo = entry.getValue();
+
+				if (ddmFieldInfo == null) {
+					obsoleteDDMFields.add(ddmField);
+
+					continue;
+				}
+
+				if (ddmField == null) {
+					ddmField = ddmFieldPersistence.create(++batchCounter);
+				}
+
+				long parentFieldId = 0;
+
+				if (ddmFieldInfo._parentInstanceId != null) {
+					parentFieldId = instanceToFieldIdMap.get(
+						ddmFieldInfo._parentInstanceId);
+				}
+
+				ddmField.setParentFieldId(parentFieldId);
+				ddmField.setStorageId(storageId);
+				ddmField.setStructureVersionId(
+					ddmStructureVersion.getStructureVersionId());
+				ddmField.setFieldName(ddmFieldInfo._fieldName);
+				ddmField.setInstanceId(ddmFieldInfo._instanceId);
+				ddmField.setPriority(priority);
+
+				if (ddmFieldInfo != rootDDMFieldInfo) {
+					DDMFormField ddmFormField = ddmFormFieldsMap.get(
+						ddmFieldInfo._fieldName);
+
+					ddmField.setFieldType(ddmFormField.getType());
+					ddmField.setLocalizable(ddmFormField.isLocalizable());
+				}
+
+				if (_ctPersistenceHelper.isInsert(ddmField)) {
+					if (!ddmField.isNew()) {
+						session.evict(
+							DDMFieldImpl.class, ddmField.getPrimaryKeyObj());
+					}
+
+					session.save(ddmField);
+				}
+				else {
+					session.merge(ddmField);
+				}
+
+				addedDDMFields.add(ddmField);
+
+				priority++;
+
+				instanceToFieldIdMap.put(
+					ddmField.getInstanceId(), ddmField.getFieldId());
 			}
 
-			if (ddmField == null) {
-				ddmField = ddmFieldPersistence.create(++batchCounter);
+			for (DDMField ddmField : addedDDMFields) {
+				_entityCache.putResult(
+					DDMFieldImpl.class, ddmField, false, true);
 			}
+		}
+		finally {
+			ddmFieldPersistence.closeSession(session);
+		}
 
-			long parentFieldId = 0;
-
-			if (ddmFieldInfo._parentInstanceId != null) {
-				parentFieldId = instanceToFieldIdMap.get(
-					ddmFieldInfo._parentInstanceId);
-			}
-
-			ddmField.setParentFieldId(parentFieldId);
-			ddmField.setStorageId(storageId);
-			ddmField.setStructureVersionId(
-				ddmStructureVersion.getStructureVersionId());
-			ddmField.setFieldName(ddmFieldInfo._fieldName);
-			ddmField.setInstanceId(ddmFieldInfo._instanceId);
-			ddmField.setPriority(priority);
-
-			if (ddmFieldInfo != rootDDMFieldInfo) {
-				DDMFormField ddmFormField = ddmFormFieldsMap.get(
-					ddmFieldInfo._fieldName);
-
-				ddmField.setFieldType(ddmFormField.getType());
-				ddmField.setLocalizable(ddmFormField.isLocalizable());
-			}
-
-			ddmField = ddmFieldPersistence.update(ddmField);
-
-			priority++;
-
-			instanceToFieldIdMap.put(
-				ddmField.getInstanceId(), ddmField.getFieldId());
+		for (DDMField ddmField : obsoleteDDMFields) {
+			ddmFieldPersistence.remove(ddmField);
 		}
 
 		if (ddmFormUpdateContext._newDDMFieldAttributesCount > 0) {
@@ -431,34 +466,70 @@ public class DDMFieldLocalServiceImpl extends DDMFieldLocalServiceBaseImpl {
 			batchCounter -= ddmFormUpdateContext._newDDMFieldAttributesCount;
 		}
 
-		for (Map.Entry<DDMFieldAttribute, DDMFieldAttributeInfo> entry :
-				ddmFormUpdateContext._ddmFieldAttributeEntries) {
+		List<DDMFieldAttribute> addedDDMFieldAttributes = new ArrayList<>();
+		List<DDMFieldAttribute> obsoleteDDMFieldAttributes = new ArrayList<>();
 
-			DDMFieldAttribute ddmFieldAttribute = entry.getKey();
-			DDMFieldAttributeInfo ddmFieldAttributeInfo = entry.getValue();
+		session = _ddmFieldAttributePersistence.openSession();
 
-			if (ddmFieldAttributeInfo == null) {
-				_ddmFieldAttributePersistence.remove(ddmFieldAttribute);
+		try {
+			for (Map.Entry<DDMFieldAttribute, DDMFieldAttributeInfo> entry :
+					ddmFormUpdateContext._ddmFieldAttributeEntries) {
 
-				continue;
+				DDMFieldAttribute ddmFieldAttribute = entry.getKey();
+				DDMFieldAttributeInfo ddmFieldAttributeInfo = entry.getValue();
+
+				if (ddmFieldAttributeInfo == null) {
+					obsoleteDDMFieldAttributes.add(ddmFieldAttribute);
+
+					continue;
+				}
+
+				if (ddmFieldAttribute == null) {
+					ddmFieldAttribute = _ddmFieldAttributePersistence.create(
+						++batchCounter);
+				}
+
+				ddmFieldAttribute.setFieldId(
+					instanceToFieldIdMap.get(
+						ddmFieldAttributeInfo._ddmFieldInfo._instanceId));
+				ddmFieldAttribute.setStorageId(storageId);
+				ddmFieldAttribute.setAttributeName(
+					ddmFieldAttributeInfo._attributeName);
+				ddmFieldAttribute.setLanguageId(
+					ddmFieldAttributeInfo._languageId);
+				ddmFieldAttribute.setAttributeValue(
+					ddmFieldAttributeInfo._attributeValue);
+
+				if (_ctPersistenceHelper.isInsert(ddmFieldAttribute)) {
+					if (!ddmFieldAttribute.isNew()) {
+						session.evict(
+							DDMFieldAttributeImpl.class,
+							ddmFieldAttribute.getPrimaryKeyObj());
+					}
+
+					session.save(ddmFieldAttribute);
+				}
+				else {
+					session.merge(ddmFieldAttribute);
+				}
+
+				addedDDMFieldAttributes.add(ddmFieldAttribute);
 			}
 
-			if (ddmFieldAttribute == null) {
-				ddmFieldAttribute = _ddmFieldAttributePersistence.create(
-					++batchCounter);
+			for (DDMFieldAttribute ddmFieldAttribute :
+					addedDDMFieldAttributes) {
+
+				_entityCache.putResult(
+					DDMFieldAttributeImpl.class, ddmFieldAttribute, false,
+					true);
 			}
+		}
+		finally {
+			_ddmFieldAttributePersistence.closeSession(session);
+		}
 
-			ddmFieldAttribute.setFieldId(
-				instanceToFieldIdMap.get(
-					ddmFieldAttributeInfo._ddmFieldInfo._instanceId));
-			ddmFieldAttribute.setStorageId(storageId);
-			ddmFieldAttribute.setAttributeName(
-				ddmFieldAttributeInfo._attributeName);
-			ddmFieldAttribute.setLanguageId(ddmFieldAttributeInfo._languageId);
-			ddmFieldAttribute.setAttributeValue(
-				ddmFieldAttributeInfo._attributeValue);
-
-			_ddmFieldAttributePersistence.update(ddmFieldAttribute);
+		for (DDMFieldAttribute ddmFieldAttribute : obsoleteDDMFieldAttributes) {
+			_ddmFieldAttributePersistence.remove(ddmFieldAttribute);
 		}
 	}
 
@@ -944,10 +1015,16 @@ public class DDMFieldLocalServiceImpl extends DDMFieldLocalServiceBaseImpl {
 		DDMFieldLocalServiceImpl.class);
 
 	@Reference
+	private CTPersistenceHelper _ctPersistenceHelper;
+
+	@Reference
 	private DDMFieldAttributePersistence _ddmFieldAttributePersistence;
 
 	@Reference
 	private DDMStructurePersistence _ddmStructurePersistence;
+
+	@Reference
+	private EntityCache _entityCache;
 
 	@Reference
 	private JSONFactory _jsonFactory;
