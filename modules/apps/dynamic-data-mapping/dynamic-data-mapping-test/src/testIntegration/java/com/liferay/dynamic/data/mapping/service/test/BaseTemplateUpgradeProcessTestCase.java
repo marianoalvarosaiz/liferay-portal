79f3de38c8c704cd082bd9ddb0e334b20f48457f
Saurasish Basak <saurasish.basak@liferay.com>
2026-01-28 17:19:29 +0530

    LPD-77080 Add Test to expose problem with several ctCollectionId
    Signed-off-by: Saurasish Basak <saurasish.basak@liferay.com>

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/service/test/BaseTemplateUpgradeProcessTestCase.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/service/test/BaseTemplateUpgradeProcessTestCase.java
@@ -5,23 +5,32 @@
 
 package com.liferay.dynamic.data.mapping.service.test;
 
+import com.liferay.change.tracking.conflict.ConflictInfo;
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTProcessLocalService;
 import com.liferay.dynamic.data.mapping.constants.DDMTemplateConstants;
 import com.liferay.dynamic.data.mapping.model.DDMTemplate;
 import com.liferay.dynamic.data.mapping.service.DDMTemplateService;
 import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryVersion;
 import com.liferay.fragment.service.FragmentCollectionService;
+import com.liferay.fragment.service.FragmentEntryLocalService;
 import com.liferay.fragment.service.FragmentEntryService;
 import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringPool;
 import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.kernel.cache.MultiVMPool;
+import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
@@ -36,7 +45,12 @@ import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 import com.liferay.portal.upgrade.test.util.UpgradeTestUtil;
 
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  * @author Albert Gomes Cabral
@@ -49,6 +63,54 @@ public abstract class BaseTemplateUpgradeProcessTestCase {
 
 		_serviceContext = ServiceContextTestUtil.getServiceContext(
 			_group.getGroupId(), TestPropsValues.getUserId());
+	}
+
+	@Test
+	public void testUpgradeFragmentEntryPreventsHtmlOverwriting()
+		throws Exception {
+
+		_ctCollection = _ctCollectionLocalService.addCTCollection(
+			null, TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
+			0, RandomTestUtil.randomString(), null);
+
+		addFragmentEntry(".v5_3_3/fragment-entry.html");
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection.getCtCollectionId())) {
+
+			_fragmentEntry.setHtml(read(".v5_3_3/updated-fragment-entry.html"));
+
+			_fragmentEntry = _fragmentEntryLocalService.updateFragmentEntry(
+				_fragmentEntry);
+		}
+
+		Map<Long, List<ConflictInfo>> conflictsMap =
+			_ctCollectionLocalService.checkConflicts(_ctCollection);
+
+		Assert.assertTrue(conflictsMap.isEmpty());
+
+		_ctProcessLocalService.addCTProcess(
+			TestPropsValues.getUserId(), _ctCollection.getCtCollectionId());
+
+		FragmentEntryVersion fragmentEntryVersion =
+			_fragmentEntryLocalService.fetchLatestVersion(_fragmentEntry);
+
+		Assert.assertEquals(2, fragmentEntryVersion.getVersion());
+
+		runTemplateUpgrade();
+
+		try (SafeCloseable safeCloseable =
+				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
+					_ctCollection.getCtCollectionId())) {
+
+			FragmentEntry fragmentEntry =
+				_fragmentEntryLocalService.getFragmentEntry(
+					_fragmentEntry.getFragmentEntryId());
+
+			Assert.assertNotEquals(
+				fragmentEntry.getHtml(), _fragmentEntry.getHtml());
+		}
 	}
 
 	protected void addDDMTemplate(String filePath) throws Exception {
@@ -119,6 +181,14 @@ public abstract class BaseTemplateUpgradeProcessTestCase {
 	)
 	private static UpgradeStepRegistrator _upgradeStepRegistrator;
 
+	private CTCollection _ctCollection;
+
+	@Inject
+	private CTCollectionLocalService _ctCollectionLocalService;
+
+	@Inject
+	private CTProcessLocalService _ctProcessLocalService;
+
 	private DDMTemplate _ddmTemplate;
 
 	@Inject
@@ -128,6 +198,9 @@ public abstract class BaseTemplateUpgradeProcessTestCase {
 	private FragmentCollectionService _fragmentCollectionService;
 
 	private FragmentEntry _fragmentEntry;
+
+	@Inject
+	private FragmentEntryLocalService _fragmentEntryLocalService;
 
 	@Inject
 	private FragmentEntryService _fragmentEntryService;
